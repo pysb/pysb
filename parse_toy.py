4d2d202be1fb760650e35c36b7f8be71b510816c
Jeremy Muhlich <jmuhlich@dhc024996.med.harvard.edu>
2008-12-03 21:20:33 -0500

    added mass action diffeq generation

--- parse_toy.py
+++ parse_toy.py
@@ -2,8 +2,8 @@ from ply import yacc, lex
 import toyyacc
 
 rules = '''
-E + S <-> ES (1,1)
-ES --> E + P (1)
+E + S <-> ES (1,2)
+ES --> E + P (0.3)
 '''
 
 model = yacc.parse(rules)
@@ -12,4 +12,10 @@ print
 print "rules:"
 for r in model.rules:
     print ' ', r
+
+model.finalize()
+
 print
+print "equations:"
+for s in model.species.values():
+    print '  d[%s]/dt = %s' % (s.name, ' + '.join(str(t) for t in s.mass_action_terms))
--- toymodels.py
+++ toymodels.py
@@ -4,22 +4,28 @@ class Model:
     def __init__(self, rules):
         self.rules = []
         self.species = {}
-
         for r in rules:
             if r:
-                r.coalesce_species(self.species)
-                new_rules = [r]
-                if r.__class__ is RuleReversible:
-                    new_rules = [
-                        RuleIrreversible(reactants=r.reactants, products=r.products, rate=r.rates[0]),
-                        RuleIrreversible(reactants=r.products, products=r.reactants, rate=r.rates[1]),
-                        ]
-                self.rules += new_rules
+                self.rules.append(r)
+
+    def finalize(self):
+        for r in self.rules:
+            new_rules = [r]
+            if r.__class__ is RuleReversible:
+                self.rules.remove(r)
+                self.rules += [
+                    RuleIrreversible(reactants=r.reactants, products=r.products, rate=r.rates[0]),
+                    RuleIrreversible(reactants=r.products, products=r.reactants, rate=r.rates[1]),
+                    ]
+        for r in self.rules:
+            r.coalesce_species(self.species)
+            r.generate_mass_action_terms()
 
 
 class Species:
     def __init__(self, name):
         self.name = name
+        self.mass_action_terms = []
 
     def __str__(self):
         return '%s' % (self.name)
@@ -29,13 +35,12 @@ class Rule:
     def coalesce_species(self, species):
         for sl in [self.reactants, self.products]:
             for i in range(len(sl)):
-                name = sl[i]
-                if name in species:
-                    s = species[name]
+                s = sl[i]
+                if s.name in species:
+                    s = species[s.name]
+                    sl[i] = s
                 else:
-                    s = Species(name)
-                    species[name] = s
-                sl[i] = s
+                    species[s.name] = s
 
 
 class RuleIrreversible(Rule):
@@ -46,10 +51,15 @@ class RuleIrreversible(Rule):
 
     def __str__(self):
         return '%s --> %s (%s)' % (
-            ' + '.join(r.id for r in self.reactants),
-            ' + '.join(p.id for p in self.products),
+            ' + '.join(r.name for r in self.reactants),
+            ' + '.join(p.name for p in self.products),
             self.rate
             )
+
+    def generate_mass_action_terms(self):
+        for t in [(self.products, self.reactants, 1), (self.reactants, self.reactants, -1)]:
+            for s in t[0]:
+                s.mass_action_terms.append(MassActionTerm(species=t[1], factor=t[2]*self.rate))
 
 
 class RuleReversible(Rule):
@@ -63,4 +73,16 @@ class RuleReversible(Rule):
             ' + '.join(str(r) for r in self.reactants),
             ' + '.join(str(p) for p in self.products),
             ', '.join(str(r) for r in self.rates)
+            )
+
+
+class MassActionTerm:
+    def __init__(self, species, factor):
+        self.species = species
+        self.factor = factor
+
+    def __str__(self):
+        return '%s*%s' % (
+            self.factor,
+            ''.join('['+str(s)+']' for s in self.species)
             )
