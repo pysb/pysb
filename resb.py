e41e082fc316f5711c1c63ee2073e873ba9ecd8b
Jeremy Muhlich <jmuhlich@bitflood.org>
2009-08-13 20:04:28 -0400

    improve monomer appearance

--- resb.py
+++ resb.py
@@ -3,12 +3,18 @@ import gtk
 import math
 
 
+corner_radius = 10
+site_radius = 5
+sites_y_pos = 35
+sites_y_spacing = 35
+
+
 class MonomerWidget(gtk.DrawingArea):
 
     def __init__(self, monomer):
         gtk.DrawingArea.__init__(self)
         self.connect("expose_event", self.expose)
-        self.set_size_request(100, 30 + 30 * len(monomer.sites))
+        self.set_size_request(100, sites_y_pos + sites_y_spacing * len(monomer.sites))
         self.monomer = monomer
 
     def expose(self, widget, event):
@@ -19,13 +25,17 @@ class MonomerWidget(gtk.DrawingArea):
         rect = self.get_allocation()
         context = self.context = self.window.cairo_create();
 
-        x = rect.width / 2
-        y = rect.height / 2
-
-        radius = min(rect.width / 2, rect.height / 2) - 5
+        context.set_line_width(1)
 
         # body
-        context.arc(x, y, radius, 0, 2 * math.pi)
+        (x1, x2) = (corner_radius + 0.5, rect.width - corner_radius - 0.5)
+        (y1, y2) = (corner_radius + 0.5, rect.height - corner_radius - 0.5)
+        angles = [v * math.pi/2.0 for v in range(0,4)]
+        context.arc(x1, y1, corner_radius, angles[2], angles[3])
+        context.arc(x2, y1, corner_radius, angles[3], angles[0])
+        context.arc(x2, y2, corner_radius, angles[0], angles[1])
+        context.arc(x1, y2, corner_radius, angles[1], angles[2])
+        context.close_path()
         context.set_source_rgb(1, 1, 1)
         context.fill_preserve()
         context.set_source_rgb(0, 0, 0)
@@ -33,23 +43,26 @@ class MonomerWidget(gtk.DrawingArea):
 
         # label
         context.set_font_size(14)
-        context.move_to(3, context.font_extents()[0] + 3)
+        context.move_to(corner_radius, context.font_extents()[0] + 3)
         context.show_text(self.monomer.name)
+        context.move_to(0, context.font_extents()[0] + 8.5)
+        context.rel_line_to(100, 0)
+        context.stroke()
 
         # sites
-        context.translate(10, 30)
+        context.translate(corner_radius, sites_y_pos)
         for site in self.monomer.sites:
             self.draw_site(site)
-            context.translate(0, 20)
+            context.translate(0, sites_y_spacing)
 
     def draw_site(self, site):
         context = self.context
         context.set_source_rgb(0, 0, 0)
-        context.move_to(5, 5)
-        context.show_text(site)
-        context.new_sub_path()
-        context.arc(0, 0, 5, 0, 2*math.pi)
+        context.arc(site_radius, 0, site_radius, 0, 2*math.pi)
         context.stroke()
+        context.move_to(0, site_radius + context.font_extents()[0])
+        context.show_text(site)
+        context.new_path()
 
 
 
