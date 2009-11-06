c1304029999ae5e8e16cd00cf42b01c85a10753c
Jeremy Muhlich <jmuhlich@bitflood.org>
2009-11-05 20:33:49 -0500

    updated test model to be compatible with latest lib code

--- model_test.py
+++ model_test.py
@@ -10,11 +10,29 @@ Compartment('membrane', dimension=2, neighbors=[extracellular, cytoplasm])
 Monomer('egf', 'R')
 Monomer('egfr', ['L', 'D', 'C'])
 
-Parameter('K_egfr_egf', 1.2)
+Parameter('Kf_egfr_egf', 1.2)
+Parameter('Kr_egfr_egf', 1.1)
 Rule('egfr_egf',
-     [egfr(L=None), egf(R=None)],
-     [egfr(L=1),    egf(R=1)],
-     K_egfr_egf)
+     egfr(L=None) + egf(R=None) <>
+     egfr(L=1)    * egf(R=1),
+     Kf_egfr_egf, Kr_egfr_egf)
+
+test.observe('free_egf', egf(R=None))
+test.observe('free_egfr', egfr(L=None))
+test.observe('bound', egf(R=1) * egfr(L=1))
 
 gen = bng.BngGenerator(model=test)
-print gen.content
+bng_content = gen.content
+bng_content += """
+begin species
+  egf(R)       6.0
+  egfr(L,D,C)  10.0
+end species
+"""
+
+if __name__ == '__main__':
+    print bng_content
+    print "begin actions"
+    print "  generate_network({overwrite=>1});"
+    print "end actions"
+
