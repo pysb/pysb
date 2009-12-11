238d2e1c6916aaed34392f354318491851493529
Jeremy Muhlich <jmuhlich@bitflood.org>
2009-12-11 00:46:01 -0500

    changed bionetgen path to /usr/local

--- test_bng.py
+++ test_bng.py
@@ -2,7 +2,7 @@ import pysb
 import pysb.bng
 import model_simple_egfr
 
-pysb.bng.pkg_path = '/home/jmuhlich/development/BioNetGen-2.0.48'
+pysb.bng.pkg_path = '/usr/local/share/bionetgen'
 
 
 ydot = pysb.bng.generate_equations(model_simple_egfr.bng_content)
