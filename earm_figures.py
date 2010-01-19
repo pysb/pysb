41ee87f1fa574437740233acc6b9ede488ccfb4b
Jeremy Muhlich <jmuhlich@bitflood.org>
2010-01-19 05:50:51 -0500

    updated figure script

--- earm_figures.py
+++ earm_figures.py
@@ -23,7 +23,8 @@ def fig_4a():
     Ts_std = [9.5,     9.5,  12.9,   7.7,  10.5]
 
     CVenv = 0.2
-    Ls = floor(logspace(1,5,3))#40))
+    # num steps was originally 40, but 15 is plenty smooth enough for screen display
+    Ls = floor(logspace(1,5,15)) 
 
     fs = empty_like(Ls)
     Ts = empty_like(Ls)
@@ -41,6 +42,7 @@ def fig_4a():
         Ts[i] = 1 / dPmax  # minutes
         Td[i] = t[ttn] / 60  # minutes
 
+    figure()
     plot(Ls/Lfactor, Td, 'g-', Ls/Lfactor, (1-CVenv)*Td, 'g--', Ls/Lfactor, (1+CVenv)*Td, 'g--')
     errorbar(Ls_exp/Lfactor, Td_exp, Td_std, None, 'go', capsize=0),
     ylabel('Td (min)'),
@@ -49,7 +51,6 @@ def fig_4a():
     a.set_xscale('log')
     a.set_xlim((min(Ls) / Lfactor, max(Ls) / Lfactor))
     a.set_ylim((0, 1000))
-    show()
 
 
 def fig_4b():
@@ -57,24 +58,26 @@ def fig_4b():
     model.reset_parameters()
     x = odesolve(model, t)
 
-    tp = t / 3600  # x axis as hours
-
-    obs = ('Bid','PARP','mSmac')
-
     x_norm = array([x['Bid'], x['PARP'], x['mSmac']]).T
-    #x_norm = array([x[s] for s in obs]).T   # alternate DRY indexing approach
     x_norm = 1 - x_norm / x_norm[0, :]  # gets away without max() since first values are largest
 
-    # this is what I originally thought 4B was plotting
+    # this is what I originally thought 4B was plotting.  oddly it's very close.
     #x_norm = array([x['tBid'], x['CPARP'], x['cSmac']]).T
     #x_norm /= x_norm.max(0)
 
-    p = plot(tp, x_norm[:,0], 'b', tp, x_norm[:,1], 'y', tp, x_norm[:,2], 'r')
-    figlegend(p, obs, 'upper left')
+    tp = t / 3600  # x axis as hours
+
+    figure()
+    plot(tp, x_norm[:,0], 'b', label='IC substrate (tBid)')
+    plot(tp, x_norm[:,1], 'y', label='EC substrate (cPARP)')
+    plot(tp, x_norm[:,2], 'r', label='MOMP (cytosolic Smac)')
+    legend(loc='upper left', bbox_to_anchor=(0,1)).draw_frame(False)
+    xlabel('Time (hr)')
+    ylabel('fraction')
     a = gca()
     a.set_ylim((-.05, 1.05))
-    show()
 
 
 fig_4a()
-#fig_4b()
+fig_4b()
+show()
