8eeaae4aec7abe470332c1f9499633dca8abcd57
Jeremy Muhlich <jmuhlich@bitflood.org>
2010-10-07 15:35:57 -0400

    added setup.py and set version (arbitrarily) to 0.1.0

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pysb',
+      version='0.1.0',
+      description='Python Systems Biology modeling system',
+      author='Jeremy Muhlich',
+      author_email='jmuhlich@bitflood.org',
+      packages=['pysb', 'pysb.generator'],
+      )
