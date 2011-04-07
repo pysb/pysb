1a8b61c22f0446560e6cf51b201d05bb41bf0008
Jeremy Muhlich <jmuhlich@bitflood.org>
2011-04-07 11:28:23 -0400

    finished converting rhs to take params as an arg

--- pysb/integrate.py
+++ pysb/integrate.py
@@ -11,14 +11,14 @@ def odesolve(model, t):
     
     # FIXME code outside of model shouldn't have to handle parameter_overrides (same for initial_conditions below)
     param_subs = dict([ (p.name, p.value) for p in model.parameters + model.parameter_overrides.values() ])
-    param_indices = # TODO: build map of param name to lexical rank
-    param_values = [param_subs[name] for name in param_names]
+    param_values = numpy.array([param_subs[p.name] for p in model.parameters])
+    param_indices = dict( (p.name, i) for i, p in enumerate(model.parameters) )
 
     c_code_eqs = '\n'.join(['ydot[%d] = %s;' % (i, sympy.ccode(model.odes[i])) for i in range(len(model.odes))])
     c_code_eqs = re.sub(r's(\d+)', lambda m: 'y[%s]' % (int(m.group(1))), c_code_eqs)
-    c_code_eqs = re.sub(r'\b(\w+)\b', lambda m: m.group(1), c_code_eqs)
-    print "EQS:\n" + c_code_eqs
-    c_code = c_code_consts + '\n\n' + c_code_eqs
+    for i, p in enumerate(model.parameters):
+        c_code_eqs = re.sub(r'\b(%s)\b' % p.name, 'p[%d]' % i, c_code_eqs)
+    c_code = c_code_eqs
 
     y0 = numpy.zeros((len(model.odes),))
     for cp, ic_param in model.initial_conditions:
@@ -28,9 +28,9 @@ def odesolve(model, t):
         si = model.get_species_index(cp)
         y0[si] = ic_param.value
 
-    def rhs(t, y, args):
+    def rhs(t, y, p):
         ydot = numpy.empty_like(y)
-        inline(c_code, ['y', 'ydot']); # sets ydot as a side effect
+        inline(c_code, ['y', 'ydot', 'p']); # sets ydot as a side effect
         return ydot
 
     nspecies = len(model.species)
@@ -40,7 +40,7 @@ def odesolve(model, t):
 
     # perform the actual integration
     integrator = ode(rhs).set_integrator('vode', method='bdf', with_jacobian=True)
-    integrator.set_initial_value(y0, t[0]).set_f_params(())
+    integrator.set_initial_value(y0, t[0]).set_f_params(param_values)
     yout[0, :nspecies] = y0  # FIXME: questionable. first computed step not necessarily continuous from y0, is it?
     i = 1
     while integrator.successful() and integrator.t < t[-1]:
