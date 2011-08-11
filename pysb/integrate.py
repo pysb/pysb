b0454a7fb4983c757675797f9d850a9a1c21b2ca
Jeremy Muhlich <jmuhlich@bitflood.org>
2011-08-11 14:17:13 -0400

    started work to make scipy.weave.inline optional

--- pysb/integrate.py
+++ pysb/integrate.py
@@ -2,8 +2,18 @@ import pysb.bng
 import numpy
 from scipy.integrate import ode
 from scipy.weave import inline
+import distutils.errors
 import sympy
 import re
+
+
+use_inline = False
+# try to inline a C statement to see if inline is functional
+try:
+    inline('int i;', force=1)
+    use_inline = True
+except distutils.errors.CompileError as e:
+    pass
 
 
 def odesolve(model, t):
@@ -13,11 +23,12 @@ def odesolve(model, t):
     param_values = numpy.array([param_subs[p.name] for p in model.parameters])
     param_indices = dict( (p.name, i) for i, p in enumerate(model.parameters) )
 
-    c_code_eqs = '\n'.join(['ydot[%d] = %s;' % (i, sympy.ccode(model.odes[i])) for i in range(len(model.odes))])
-    c_code_eqs = re.sub(r's(\d+)', lambda m: 'y[%s]' % (int(m.group(1))), c_code_eqs)
-    for i, p in enumerate(model.parameters):
-        c_code_eqs = re.sub(r'\b(%s)\b' % p.name, 'p[%d]' % i, c_code_eqs)
-    c_code = c_code_eqs
+    if use_inline:
+        c_code_eqs = '\n'.join(['ydot[%d] = %s;' % (i, sympy.ccode(model.odes[i])) for i in range(len(model.odes))])
+        c_code_eqs = re.sub(r's(\d+)', lambda m: 'y[%s]' % (int(m.group(1))), c_code_eqs)
+        for i, p in enumerate(model.parameters):
+            c_code_eqs = re.sub(r'\b(%s)\b' % p.name, 'p[%d]' % i, c_code_eqs)
+        c_code = c_code_eqs
 
     y0 = numpy.zeros((len(model.odes),))
     for cp, ic_param in model.initial_conditions:
@@ -26,7 +37,10 @@ def odesolve(model, t):
 
     def rhs(t, y, p):
         ydot = numpy.empty_like(y)
-        inline(c_code, ['y', 'ydot', 'p']); # sets ydot as a side effect
+        if use_inline:
+            inline(c_code, ['y', 'ydot', 'p']); # sets ydot as a side effect
+        else:
+            raise Exception("Pure-python RHS not yet implemented.")  # FIXME implement it
         return ydot
 
     nspecies = len(model.species)
