10e79634c080120670c8c49c56d1a77dd6d69fdf
Jeremy Muhlich <jmuhlich@bitflood.org>
2012-06-07 16:36:43 -0400

    added numpydoc sphinx extension

--- doc/conf.py
+++ doc/conf.py
@@ -18,6 +18,7 @@ import re
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.abspath('ext'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -26,7 +27,8 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.ifconfig']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.coverage',
+              'sphinx.ext.pngmath', 'sphinx.ext.ifconfig', 'numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
