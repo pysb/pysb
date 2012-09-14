346d1ae60a720b934e59b88eeacd5b48f22da128
Jeremy Muhlich <jmuhlich@bitflood.org>
2012-09-14 16:47:20 -0400

    switch to using external numpydoc package

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,6 @@ import re
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
-sys.path.append(os.path.abspath('ext'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -28,7 +27,8 @@ sys.path.append(os.path.abspath('ext'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.coverage',
-              'sphinx.ext.pngmath', 'sphinx.ext.ifconfig', 'numpydoc', 'sphinx.ext.viewcode']
+              'sphinx.ext.pngmath', 'sphinx.ext.ifconfig', 'numpydoc',
+              'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -218,3 +218,7 @@ man_pages = [
     ('index', 'pysb', u'pysb Documentation',
      [u'Jeremy Muhlich'], 1)
 ]
+
+# -- Options for numpydoc ------------------------------------------------------
+
+numpydoc_show_class_members = False
