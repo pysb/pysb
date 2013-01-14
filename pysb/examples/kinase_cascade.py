03102f1e39bc24c045f1e76f9fad7214f8a31509
Jeremy Muhlich <jmuhlich@bitflood.org>
2013-01-14 16:23:12 -0500

    switch to a hierarchy of macros in kinase cascade example

--- pysb/examples/kinase_cascade.py
+++ pysb/examples/kinase_cascade.py
@@ -11,7 +11,7 @@ Implemented by: Jeremy Muhlich
 """
 
 from pysb import *
-from pysb.macros import catalyze
+from pysb.macros import catalyze_state
 
 Model()
 
@@ -45,38 +45,31 @@ klist_bind = [kf_bind, kr_bind]
 klist_phos = klist_bind + [kcat_phos]
 klist_dephos = klist_bind + [kcat_dephos]
 
-catalyze(Ras, 'k', Raf(s='u'), 's', Raf(s='p'), klist_phos)
-catalyze(PP2A, 'ppt', Raf(s='p', k=None), 's', Raf(s='u', k=None), klist_dephos)
+def mapk_single(kinase, pptase, substrate, site):
+    """Kinase phos/dephosphorylation."""
+    ppt_substrate = substrate()
+    if 'k' in ppt_substrate.monomer.sites:
+        # Ensure substrates which are themselves kinases don't get
+        # dephosphorylated while they are bound to *their* substrate.
+        ppt_substrate = ppt_substrate(k=None)
+    components = catalyze_state(kinase, 'k',
+                                substrate, site, site, 'u', 'p',
+                                klist_phos)
+    components |= catalyze_state(pptase, 'ppt',
+                                 ppt_substrate, site, site, 'p', 'u',
+                                 klist_dephos)
+    return components
 
-# Phosphorylation/dephosphorylation of MEK by Raf/PP2A
-# (this implements sequential (not independent) (de)phosphorylation)
-catalyze(Raf(s='p'), 'k',
-         MEK(s218='u'), 's218',
-         MEK(s218='p'), klist_phos)
-catalyze(Raf(s='p'), 'k',
-         MEK(s218='p', s222='u'), 's222',
-         MEK(s218='p', s222='p'), klist_phos)
-catalyze(PP2A, 'ppt',
-         MEK(s218='p', s222='u', k=None), 's218',
-         MEK(s218='u', s222='u', k=None), klist_dephos)
-catalyze(PP2A, 'ppt',
-         MEK(s218='p', s222='p', k=None), 's222',
-         MEK(s218='p', s222='u', k=None), klist_dephos)
+def mapk_double(kinase, pptase, substrate, site1, site2):
+    """Distributive + ordered double kinase phos/dephosphorylation."""
+    components = mapk_single(kinase, pptase, substrate({site2: 'u'}), site1)
+    components |= mapk_single(kinase, pptase, substrate({site1: 'p'}), site2)
+    return components
 
-# Phosphorylation/dephosphorylation of ERK by MEK/MKP
-# (also sequential)
-catalyze(MEK(s218='p', s222='p'), 'k',
-         ERK(t185='u'), 't185',
-         ERK(t185='p'), klist_phos)
-catalyze(MEK(s218='p', s222='p'), 'k',
-         ERK(t185='p', y187='u'), 'y187',
-         ERK(t185='p', y187='p'), klist_phos)
-catalyze(MKP, 'ppt',
-         ERK(t185='p', y187='u'), 't185',
-         ERK(t185='u', y187='u'), klist_dephos)
-catalyze(MKP, 'ppt',
-         ERK(t185='p', y187='p'), 'y187',
-         ERK(t185='p', y187='u'), klist_dephos)
+# Ras-Raf-MEK-ERK kinase cascade
+mapk_single(Ras, PP2A, Raf, 's')
+mapk_double(Raf(s='p'), PP2A, MEK, 's218', 's222')
+mapk_double(MEK(s218='p', s222='p'), MKP, ERK, 't185', 'y187')
 
 Initial(Ras(k=None), Parameter('Ras_0', 6e4))
 Initial(Raf(s='u', k=None), Parameter('Raf_0', 7e4))
