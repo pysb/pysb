37250b892e2bf54915a4c0039ab7b9ea54e8fcc2
Leonard A. Harris <lh64@cornell.edu>
2014-09-08 20:02:37 -0500

    Modifying tyson_oscillator.py to be identical to that in the 'tropical' branch

--- pysb/examples/tyson_oscillator.py
+++ pysb/examples/tyson_oscillator.py
@@ -1,5 +1,3 @@
-# 1991, Tyson, Modeling the cell division cycle: cdc2 and cyclin interactions
-
 from pysb import *
 from pysb.macros import *
 
@@ -9,26 +7,26 @@ NA = Parameter("NA", 6.0221413e23)
 V = Parameter("VOL", 1e-20)
 NA_V = NA.value*V.value
 
-Parameter('k1',  0.015*NA_V)
-# Parameter('k1',  0.015)
+Parameter('k1', 0.015*NA_V)
+# Parameter('k1', 0.015)
 
-Parameter('k2',  0)
+Parameter('k2', 0)
 
-Parameter('k3',  200/NA_V)
-# Parameter('k3',  200)
+Parameter('k3', 200/NA_V)
+# Parameter('k3', 200)
 
-Parameter('k4',  2*180/NA_V/NA_V)
-# Parameter('k4',  2*180)
+Parameter('k4', 2*180/NA_V/NA_V)
+# Parameter('k4', 2*180)
 
 Parameter('kp4', 0.018)
-Parameter('k5',  0)
-Parameter('k6',  1.0)
-Parameter('k7',  0.6)
-Parameter('k8',  1e6) #1e12)
-Parameter('k9',  1e3) #1e6)
+Parameter('k5', 0)
+Parameter('k6', 1.0)
+Parameter('k7', 0.6)
+Parameter('k8', 1e6) #1e12)
+Parameter('k9', 1e3) #1e6)
 
 Monomer('cyclin', ['Y', 'b'], {'Y': ['U','P']})
-Monomer('cdc2',   ['Y', 'b'], {'Y': ['U','P']})
+Monomer('cdc2', ['Y', 'b'], {'Y': ['U','P']})
 
 # Rule 1
 synthesize(cyclin(Y='U', b=None), k1)
@@ -44,24 +42,22 @@ Rule('Activation', cyclin(Y='P', b=1) % cdc2(Y='P', b=1) >> cyclin(Y='P', b=1) %
 
 # Rule 4'
 Rule('Autocatalytic', cyclin(Y='P', b=1) % cdc2(Y='P', b=1) + cyclin(Y='P', b=2) % cdc2(Y='U', b=2) + cyclin(Y='P', b=2) % cdc2(Y='U', b=2) >>
-                      cyclin(Y='P', b=1) % cdc2(Y='U', b=1) + cyclin(Y='P', b=2) % cdc2(Y='U', b=2) + cyclin(Y='P', b=2) % cdc2(Y='U', b=2), k4)
+cyclin(Y='P', b=1) % cdc2(Y='U', b=1) + cyclin(Y='P', b=2) % cdc2(Y='U', b=2) + cyclin(Y='P', b=2) % cdc2(Y='U', b=2), k4)
 
 # Rule 5
-#Rule('Opposed', cyclin(Y='P', b=1) % cdc2(Y='U', b=1)  >> cyclin(Y='P', b=1) % cdc2(Y='P', b=1), k5)
+#Rule('Opposed', cyclin(Y='P', b=1) % cdc2(Y='U', b=1) >> cyclin(Y='P', b=1) % cdc2(Y='P', b=1), k5)
 
 # Rule 6
 #Rule('Dissociation', cyclin(Y='P', b=1) % cdc2(Y='U', b=1) >> cyclin(Y='P', b=None) + cdc2(Y='U', b=None), k6)
 Rule('Dissociation', cyclin(Y='P', b=1) % cdc2(Y='U', b=1) >> cdc2(Y='U', b=None), k6)
-
 # Rule 7
 #degrade(cyclin(Y='P', b=None), k7)
-
 # Rule 8 and 9
 equilibrate(cdc2(Y='U', b=None), cdc2(Y='P', b=None), [k8, k9])
 
-Observable("YT", cyclin())                                # Total Cyclin
-Observable("CT", cdc2())                                  # Total CDC2
-#Observable("M",  cyclin(Y='P', b=1) % cdc2(Y='U', b=1) )  # Active Complex
+Observable("YT", cyclin()) # Total Cyclin
+Observable("CT", cdc2()) # Total CDC2
+#Observable("M", cyclin(Y='P', b=1) % cdc2(Y='U', b=1) ) # Active Complex
 
 Observable("Y1", cdc2(Y='U', b=None))
 Observable("Y2", cdc2(Y='P', b=None))
@@ -70,8 +66,8 @@ Observable("Y4", cdc2(Y='P', b=1) % cyclin(Y='P', b=1))
 Observable("Y5", cyclin(Y='U', b=None))
 
 #Observable("CYCLIN_P", cyclin(Y='P', b=None))
-
 # [C2] in Tyson
+
 Parameter("cdc0", 1*NA_V)
 # Parameter("cdc0", 1.0)
 Initial(cdc2(Y='P', b=None), cdc0)
@@ -80,4 +76,3 @@ Initial(cdc2(Y='P', b=None), cdc0)
 Parameter('cyc0', 0.25*NA_V)
 # Parameter('cyc0', 0.25)
 Initial(cyclin(Y='U', b=None), cyc0)
-
