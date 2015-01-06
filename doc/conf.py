65ea1fd63c1e813d04069fe4dfbd0400c2946316
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2015-01-06 17:44:14 -0500

    Add sphinx.ext.autosummary to conf.py
    sphinx otherwise does not understand ..autosummary.

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.coverage',
               'sphinx.ext.pngmath', 'sphinx.ext.ifconfig', 'numpydoc',
-              'sphinx.ext.viewcode']
+              'sphinx.ext.viewcode', 'sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
