a4b698cfc50ace961f70d05bd4b2edfd591c06b5
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2015-01-07 13:13:35 -0500

    py3: apply 'func_attrs' fixer

--- pysb/testing/__init__.py
+++ pysb/testing/__init__.py
@@ -5,11 +5,11 @@ import pickle
 def with_model(func):
     """Decorate a test to set up and tear down a Model."""
     def inner(*args, **kwargs):
-        model = Model(func.func_name, _export=False)
+        model = Model(func.__name__, _export=False)
         # manually set up SelfExporter, targeting func's globals
         SelfExporter.default_model = model
         SelfExporter.target_module = func.__module__
-        SelfExporter.target_globals = func.func_globals
+        SelfExporter.target_globals = func.__globals__
         SelfExporter.target_globals['model'] = model
         try:
             # call the actual test function
