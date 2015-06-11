c58c399d807c54720f8a1807db678b42edf43dcf
John Bachman <bachmanjohn@gmail.com>
2015-06-11 01:54:52 -0400

    Tests of the Solver on Robertson and EARM 1.0
    These Tests minimally ensure that there are no errors (i.e., crashes) in
    the code paths to prepare the RHS and Jacobian functions and run.

--- pysb/tests/test_integrate.py
+++ pysb/tests/test_integrate.py
@@ -1,9 +1,26 @@
 from pysb.integrate import Solver
-from pysb.examples.robertson import model
+from pysb.examples import robertson, earm_1_0
 import numpy as np
 
 def test_robertson_integration():
     t = np.linspace(0, 100)
-    sol = Solver(model, t)
+    # Run with and without inline
+    Solver._use_inline = False
+    sol = Solver(robertson.model, t, use_jacobian=True)
     sol.run()
     assert sol.y.shape[0] == t.shape[0]
+    # Run with and without inline
+    Solver._use_inline = True
+    sol = Solver(robertson.model, t, use_jacobian=True)
+    sol.run()
+
+def test_earm_integration():
+    t = np.linspace(0, 1e4, 1000)
+    # Run with and without inline
+    Solver._use_inline = False
+    sol = Solver(earm_1_0.model, t, use_jacobian=True)
+    sol.run()
+    Solver._use_inline = True
+    sol = Solver(earm_1_0.model, t, use_jacobian=True)
+    sol.run()
+
