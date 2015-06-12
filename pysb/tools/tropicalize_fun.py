186bc865d7b0bd32cb3952c3bd58c727aaaf9b04
Leonard A. Harris <lh64@cornell.edu>
2015-06-11 23:59:48 -0500

    Changed path to earm to pysb.examples.earm_2_0.lopez_embedded in tropicalize_fun.py

--- pysb/tools/tropicalize_fun.py
+++ pysb/tools/tropicalize_fun.py
@@ -330,7 +330,7 @@ def range_dominating_monomials(model, t, only_observables = True):
 
 
 # from pysb.examples.tyson_oscillator import model
-from earm.lopez_embedded import model
+from pysb.examples.earm_2_0.lopez_embedded import model
 t= numpy.linspace(0, 10000, 10001)          # timerange used
 range_dominating_monomials(model, t) 
 
