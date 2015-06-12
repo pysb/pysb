2abf23077177346812d1167ac69b18793c8115b8
Leonard A. Harris <lh64@cornell.edu>
2015-06-12 16:43:09 -0500

    Doing some general code cleanup: 1) removing the 'examples/earm_2_0' directory; 2) commenting out earm calls from tropicalize.py and tropicalize_fun.py; 3) deleting varsens.py from 'pysb/tools'; 4) removing the 'ghalton' dependency from .travis.yml.

--- pysb/tools/tropicalize.py
+++ pysb/tools/tropicalize.py
@@ -332,11 +332,11 @@ class Tropical:
              
 
 
-#from pysb.examples.tyson_oscillator import model
-from pysb.examples.earm_2_0.lopez_embedded import model
-t= numpy.linspace(0, 20000, 20001)          # timerange used
-tro = Tropical(model)
-tro.tropicalize(t)
+# from pysb.examples.tyson_oscillator import model
+# # from earm.lopez_embedded import model
+# t= numpy.linspace(0, 20000, 20001)          # timerange used
+# tro = Tropical(model)
+# tro.tropicalize(t)
 #tro.final_tropicalization() 
 
 ######################################################################## Change of parameters
