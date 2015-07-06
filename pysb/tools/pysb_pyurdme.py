af16ea1334c9cd1489b6b6c1929e6c23232af95d
Alex Lubbock <code@alexlubbock.com>
2015-07-06 11:48:09 -0500

    Fix import warnings for missing packages

--- pysb/tools/pysb_pyurdme.py
+++ pysb/tools/pysb_pyurdme.py
@@ -1,7 +1,8 @@
 try:
     import pyurdme
-except:
-    warning("Package 'pyurdme' cannot be found and is required for pyurdme simulations. See XXX for further details.")
+except ImportError:
+    import warnings
+    warnings.warn("Package 'pyurdme' cannot be found and is required for pyurdme simulations. See XXX for further details.")
 from pysb.simulate import Simulator
 from pysb.bng import generate_equations
 import re
--- pysb/tools/stochkit.py
+++ pysb/tools/stochkit.py
@@ -1,7 +1,8 @@
 try:
     import gillespy
 except:
-    warning("Package 'gillespy' cannot be found and is required for StochKit simulations. See XXX for further details.")
+    import warnings
+    warnings.warn("Package 'gillespy' cannot be found and is required for StochKit simulations. See XXX for further details.")
 from pysb.simulate import Simulator
 from pysb.bng import generate_equations
 import re
