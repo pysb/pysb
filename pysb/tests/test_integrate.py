74e4c65a880fa430b5df562298e7bc0c92c21e63
jamespino <james.c.pino@vanderbilt.edu>
2015-08-01 19:32:44 -0500

    added tests for param_values and y0 as dictionaries

--- pysb/tests/test_integrate.py
+++ pysb/tests/test_integrate.py
@@ -5,6 +5,13 @@ from pysb.integrate import odesolve
 from pylab import linspace, plot, xlabel, ylabel, show
 from sympy import sympify
 
+from pysb import *
+from pysb.integrate import *
+from pysb.bng import run_ssa
+from pysb.macros import synthesize
+import matplotlib.pyplot as plt
+import numpy as np
+
 @with_model
 def test_integrate_with_expression():
 
@@ -32,3 +39,58 @@ def test_integrate_with_expression():
     time = linspace(0, 40, 100)
     x = odesolve(model, time, verbose=True)
     
+@with_model
+def test_y0_as_dict():
+
+    Monomer('A', ['a'])
+    Monomer('B', ['b'])
+    
+    Parameter('ksynthA', 100)
+    Parameter('ksynthB', 100)
+    Parameter('kbindAB', 100)
+    
+    Parameter('A_init', 0)
+    Parameter('B_init', 0)
+    
+    Initial(A(a=None), A_init)
+    Initial(B(b=None), B_init)
+    
+    Observable("A_free", A(a=None))
+    Observable("B_free", B(b=None))
+    Observable("AB_complex", A(a=1) % B(b=1))
+    
+    synthesize(A(a=None), ksynthA)
+    synthesize(B(b=None), ksynthB)
+    Rule('AB_bind', A(a=None) + B(b=None) >> A(a=1) % B(b=1), kbindAB)
+    
+    time = np.linspace(0, 0.005, 101)
+    solver = Solver(model, time, verbose=True)
+    solver.run(y0={A(a=None):100, B(b=1) % A(a=1):100, B(b=None):50})
+
+@with_model
+def test_param_values_as_dict():
+
+    Monomer('A', ['a'])
+    Monomer('B', ['b'])
+    
+    Parameter('ksynthA', 100)
+    Parameter('ksynthB', 100)
+    Parameter('kbindAB', 100)
+    
+    Parameter('A_init', 0)
+    Parameter('B_init', 0)
+    
+    Initial(A(a=None), A_init)
+    Initial(B(b=None), B_init)
+    
+    Observable("A_free", A(a=None))
+    Observable("B_free", B(b=None))
+    Observable("AB_complex", A(a=1) % B(b=1))
+    
+    synthesize(A(a=None), ksynthA)
+    synthesize(B(b=None), ksynthB)
+    Rule('AB_bind', A(a=None) + B(b=None) >> A(a=1) % B(b=1), kbindAB)
+    
+    time = np.linspace(0, 0.005, 101)
+    solver = Solver(model, time, verbose=True)
+    solver.run(param_values={'ksynthA':150})
