149c33123c26d7ab5b2fe7ee9d37b539a1e17034
Leonard A. Harris <lh64@cornell.edu>
2015-08-27 19:46:38 -0400

    Adding 'examples/run_tyson_cupsoda.py'. Right now, it just runs 100 simulations with the same parameters and initial concentrations. May want to change this in the future but for now it's good enough.

---
+++ examples/run_tyson_cupsoda.py
@@ -0,0 +1,29 @@
+from pysb.examples.tyson_oscillator import model
+from pysb_cupsoda import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+tspan = np.linspace(0, 500, 5001)
+
+set_cupSODA_path("/Users/lopezlab/cupSODA") #FIXME: should search for cupSODA in standard locations
+solver = CupSODASolver(model, tspan, atol=1e-12, rtol=1e-6, max_steps=20000, verbose=True)
+
+n_sims = 100
+
+# Rate constants
+param_values = np.ones((n_sims, len(model.parameters_rules())))
+for i in range(len(param_values)):
+    for j in range(len(param_values[i])):
+        param_values[i][j] *= model.parameters_rules()[j].value
+
+# Initial concentrations
+y0 = np.zeros((n_sims, len(model.species)))
+for i in range(len(y0)):
+    for ic in model.initial_conditions:
+        for j in range(len(y0[i])):
+            if str(ic[0]) == str(model.species[j]):
+                y0[i][j] = ic[1].value
+                break
+
+solver.run(param_values, y0) #, outdir=os.path.join(outdir,'NSAMPLES_'+str(n_samples))) #obs_species_only=False, load_conc_data=False)
+        
