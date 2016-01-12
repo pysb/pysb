8a0acaf18eea3ea23117a1656babb80f8c0e5869
John Bachman <bachmanjohn@gmail.com>
2016-01-12 15:56:19 -0500

    Remove test of export
    Kasim execution on generated models is already tested in the other tests
    that call run_kasim. pysb.export functionality, if it is to be tested,
    should be done separately for all formats.

--- pysb/tests/test_kappa.py
+++ pysb/tests/test_kappa.py
@@ -36,29 +36,6 @@ def test_kappa_wild():
     Rule('deg_A', A(site=pysb.WILD) >> None, Parameter('k', 1))
     ok_(run_kasim(model, time=0, cleanup=True))
 
-def test_exported_kappa_file():
-    m = """from pysb import *
-Model()
-Monomer('A',['site'],{'site': ['u']})
-Parameter('two',2)
-Parameter('kr',0.1)
-Parameter('num_A',1000)
-Expression('kf',1e-5/two)
-Initial(A(site=('u')),num_A)
-Rule('dimerize_f',A(site='u')+A(site='u')>>A(site=('u',1))%A(site=('u',1)), kf)
-Rule('dimerize_r',A(site=('u',1))%A(site=('u',1))>>A(site='u')+A(site='u'), kr)
-Rule('degrade_dimer',A(site=('u',ANY)) >> None,kr)
-Observable('dimer',A(site=('u',ANY)))
-Observable('total_A_patterns',A(site=('u',WILD)))"""
-    wf = open('test.py','w')
-    wf.write(m)
-    wf.close()
-    commands =\
-           'python -m pysb.export test.py kappa > test.ka; KaSim -i test.ka -e 0'
-    # tests kappa WILD pattern and kappa syntax generation
-    ok_(subprocess.check_call(commands, shell=True) == 0)
-    subprocess.call('rm test.ka; rm test.py*', shell=True)
-
 @with_model
 def test_contact_map_kasa():
     Monomer('A', ['b'])
