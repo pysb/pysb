702af872adfd426d56f7cb397c2d071c06d87ec1
Jeremy Muhlich <jmuhlich@bitflood.org>
2016-03-31 09:56:02 -0400

    Clean up integrator tests.
    * Remove unused imports
    * Make test class *not* a subclass of TestCase and rename it so nose sees it
    * Only build the default solver in TestSolver.setUp and also add tearDown
    * Change time range in TestSolver so it actually triggers jacobian evaluation
    * Clean up docstrings
    * Change to numpy.allclose for comparing numeric results to be more explicit
      about what's being done
    * Add vode + analytic_jacobian test
    * Drop analytic_jacobian from earm and robertson general simulation tests as
      generating the jacobian is very slow.

--- pysb/tests/test_integrate.py
+++ pysb/tests/test_integrate.py
@@ -1,24 +1,16 @@
 from pysb.testing import *
-from pysb.core import *
-from pysb.macros import *
 from pysb.integrate import odesolve, Solver
-from pylab import linspace, plot, xlabel, ylabel, show
-from sympy import sympify
 import numpy as np
-from pysb import *
+from pysb import Monomer, Parameter, Initial, Observable, Rule, Expression
 from pysb.bng import run_ssa
-from pysb.macros import synthesize
-import matplotlib.pyplot as plt
-from unittest import TestCase
 
 from pysb.examples import robertson, earm_1_0
 
 
-class SolverTests(TestCase):
+class TestSolver(object):
+
     @with_model
     def setUp(self):
-        """ Setup for Solver() tests """
-
         Monomer('A', ['a'])
         Monomer('B', ['b'])
 
@@ -36,75 +28,87 @@ class SolverTests(TestCase):
         Observable("B_free", B(b=None))
         Observable("AB_complex", A(a=1) % B(b=1))
 
-        synthesize(A(a=None), ksynthA)
-        synthesize(B(b=None), ksynthB)
+        Rule('A_synth', None >> A(a=None), ksynthA)
+        Rule('B_synth', None >> B(b=None), ksynthB)
         Rule('AB_bind', A(a=None) + B(b=None) >> A(a=1) % B(b=1), kbindAB)
 
-        time = np.linspace(0, 0.005, 101)
-        self.solver = Solver(model, time)
-        self.solver_lsoda = Solver(model, time, integrator='lsoda')
-        self.solver_lsoda_jac = Solver(model, time, integrator='lsoda',
-                                       use_analytic_jacobian=True)
+        self.model = model
 
-    def test_solver_run(self):
-        """ Test solver.run() with no arguments """
+        # This timespan is chosen to be enough to trigger a Jacobian evaluation
+        # on the various solvers.
+        self.time = np.linspace(0, 1)
+        self.solver = Solver(self.model, self.time, integrator='vode')
+
+    def tearDown(self):
+        self.model = None
+        self.time = None
+        self.solver = None
+
+    def test_vode_solver_run(self):
+        """Test vode."""
         self.solver.run()
-        
+
+    def test_vode_jac_solver_run(self):
+        """Test vode and analytic jacobian."""
+        solver_vode_jac = Solver(self.model, self.time, integrator='vode',
+                                  use_analytic_jacobian=True)
+        solver_vode_jac.run()
+
     def test_lsoda_solver_run(self):
-        """ Test solver.run() with no arguments """
-        self.solver_lsoda.run()
-    
+        """Test lsoda."""
+        solver_lsoda = Solver(self.model, self.time, integrator='lsoda')
+        solver_lsoda.run()
+
     def test_lsoda_jac_solver_run(self):
-        """ Test solver.run() with no arguments """
-        self.solver_lsoda_jac.run()
-        
+        """Test lsoda and analytic jacobian."""
+        solver_lsoda_jac = Solver(self.model, self.time, integrator='lsoda',
+                                  use_analytic_jacobian=True)
+        solver_lsoda_jac.run()
+
     @raises(NotImplementedError)
     def test_y0_as_dictionary_monomer_species(self):
-        """ Test solver.run() with y0 as a dictionary containing monomers
-            defined within model """
+        """Test y0 with model-defined species."""
         self.solver.run(y0={"A(a=None)": 10, "B(b=1) % A(a=1)": 0,
                         "B(b=None)": 0})
-        assert np.abs(self.solver.y[0,0] - 10) < 1e-8
+        assert np.allclose(self.solver.y[0, 0], 10)
 
     @raises(NotImplementedError)
     def test_y0_as_dictionary_with_bound_species(self):
-        """ Test solver.run() with y0 as a dictionary containing a complex
-        that is generated with BioNetGen """
+        """Test y0 with dynamically generated species."""
         self.solver.run(y0={"A(a=None)": 0, "B(b=1) % A(a=1)": 100,
                         "B(b=None)": 0})
-        assert np.abs(self.solver.y[0, 3] - 100) < 1e-8
+        assert np.allclose(self.solver.y[0, 3], 100)
 
     @raises(NotImplementedError)
     def test_y0_invalid_dictionary_key(self):
-        """ Test solver.run() using y0 dictionary with invalid monomer name """
+        """Test y0 with invalid monomer name."""
         self.solver.run(y0={"C(c=None)": 1})
 
     @raises(NotImplementedError)
     def test_y0_non_numeric_value(self):
-        """ Test solver.run() using y0 dictionary with a non-numeric value """
+        """Test y0 with non-numeric value."""
         self.solver.run(y0={"A(a=None)": 'eggs'})
 
     def test_param_values_as_dictionary(self):
-        """ Test solver.run() with param_values as a dictionary """
+        """Test param_values as a dictionary."""
         self.solver.run(param_values={'kbindAB': 0})
-        # kbindAB=0 should ensure no AB_complex is produced
-        assert all(self.solver.yobs["AB_complex"] < 1e-8)
+        # kbindAB=0 should ensure no AB_complex is produced.
+        assert np.allclose(self.solver.yobs["AB_complex"], 0)
 
     @raises(IndexError)
     def test_param_values_invalid_dictionary_key(self):
-        """ Test solver.run() using param_values dictionary with invalid
-               parameter name """
+        """Test param_values with invalid parameter name."""
         self.solver.run(param_values={'spam': 150})
 
     @raises(ValueError, TypeError)
     def test_param_values_non_numeric_value(self):
-        """ Test solver.run() using param_values dict with a non-numeric
-               value """
+        """Test param_values with non-numeric value."""
         self.solver.run(param_values={'ksynthA': 'eggs'})
 
 
 @with_model
 def test_integrate_with_expression():
+    """Ensure a model with Expressions simulates."""
 
     Monomer('s1')
     Monomer('s9')
@@ -128,46 +132,46 @@ def test_integrate_with_expression():
     Rule('R2', None >> s20(), ks0)
     Rule('R3', s16() + s20() >> s16() + s1(), keff)
 
-    time = linspace(0, 40, 100)
+    time = np.linspace(0, 40)
     x = odesolve(model, time)
 
 
 def test_robertson_integration():
+    """Ensure robertson model simulates."""
     t = np.linspace(0, 100)
     # Run with or without inline
-    sol = Solver(robertson.model, t, use_analytic_jacobian=True)
+    sol = Solver(robertson.model, t)
     sol.run()
     assert sol.y.shape[0] == t.shape[0]
-
     if Solver._use_inline:
         # Also run without inline
         Solver._use_inline = False
-        sol = Solver(robertson.model, t, use_analytic_jacobian=True)
+        sol = Solver(robertson.model, t)
         sol.run()
         assert sol.y.shape[0] == t.shape[0]
         Solver._use_inline = True
 
 
 def test_earm_integration():
-    t = np.linspace(0, 1e3, 1000)
+    """Ensure earm_1_0 model simulates."""
+    t = np.linspace(0, 1e3)
     # Run with or without inline
-    sol = Solver(earm_1_0.model, t, use_analytic_jacobian=True)
+    sol = Solver(earm_1_0.model, t)
     sol.run()
     if Solver._use_inline:
         # Also run without inline
         Solver._use_inline = False
-        sol = Solver(earm_1_0.model, t, use_analytic_jacobian=True)
+        sol = Solver(earm_1_0.model, t)
         sol.run()
         Solver._use_inline = True
 
 
 def test_run_ssa():
-    """
-    Rudimentary test, mainly to ensure API compatibility
-    """
+    """Test run_ssa."""
     run_ssa(robertson.model, t_end=20000, n_steps=100, verbose=False)
 
 
 @raises(UserWarning)
 def test_nonexistent_integrator():
+    """Ensure nonexistent integrator raises."""
     Solver(robertson.model, np.linspace(0, 1, 2), integrator='does_not_exist')
