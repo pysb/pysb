716efdb88ec11b90f57fb06e73397fa5d3aa139e
Alex Lubbock <code@alexlubbock.com>
2016-04-21 23:30:07 -0500

    BioNetGen installation script, with setuptools --install-bng option
    This adds a script for downloading and installing BioNetGen into the location
    expected by PySB (or any other specified location). This option reduces
    hassle for new users, facilitates cross-platform unit testing (by installing
    the correct BNG binary for the current platform) and moves us closer to a
    one step install: `pip install --install-option="--install-bng" pysb`.
    
    Untested on Windows as yet.

--- setup.py
+++ setup.py
@@ -1,20 +1,44 @@
 from ez_setup import use_setuptools
 use_setuptools()
 from setuptools import setup
+from setuptools.command.install import install
 import setuptools.command.build_py
-
 import versioneer
-
 import sys, os, subprocess, re
+from scripts.install_bng import install_bng
+
 
 class build_py(setuptools.command.build_py.build_py):
     # Simplest way to use a specific list of fixers. Note use_2to3_fixers will
     # be ignored.
     fixer_names = ['lib2to3.fixes.fix_ne']
 
+
+class PysbInstaller(install):
+    user_options = install.user_options
+    user_options.append(('install-bng', None, 'Download and install '
+                                              'BioNetGen (requires write '
+                                              'access to /usr/local/share on '
+                                              'Mac or Linux, or c:\\Program '
+                                              'Files on Windows). An '
+                                              'error will occur if a '
+                                              'directory called BioNetGen '
+                                              'exists in the above locations '
+                                              'already.'))
+
+    def initialize_options(self):
+        install.initialize_options(self)
+        self.install_bng = None
+
+    def run(self):
+        install.run(self)
+        if self.install_bng:
+            install_bng()
+
+
 def main():
 
-    cmdclass = {'build_py': build_py}
+    cmdclass = {'build_py': build_py, 'install': PysbInstaller}
     cmdclass.update(versioneer.get_cmdclass())
 
     setup(name='pysb',
