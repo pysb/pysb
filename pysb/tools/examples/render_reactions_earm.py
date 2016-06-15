b7b295c11723e8e5efdc25580021bc7954970874
oscar <oo.ortega2247@gmail.com>
2016-06-15 13:17:46 -0500

    change arrow shape for modifiers

--- pysb/tools/examples/render_reactions_earm.py
+++ pysb/tools/examples/render_reactions_earm.py
@@ -1,4 +1,4 @@
 from pysb.tools.render_models_cytoscape import run_render_model
-from earm.lopez_embedded import model
+from pysb.examples.tyson_oscillator import model
 
 run_render_model(model, render_type='reactions')
