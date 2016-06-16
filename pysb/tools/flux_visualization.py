ee6d9d47f7e63a7a39824210bfbe4c34c8ad4b4a
oscar <oo.ortega2247@gmail.com>
2016-06-16 17:22:52 -0500

    Write some documentation for the class

--- pysb/tools/flux_visualization.py
+++ pysb/tools/flux_visualization.py
@@ -14,13 +14,16 @@ from py2cytoscape.data.util_network import NetworkUtil as util
 from collections import OrderedDict
 from py2cytoscape.data.cyrest_client import CyRestClient
 
+# Cytoscape session
+cy = CyRestClient()
+cy.session.delete()
 
+
+# Networkx Digraph that stores the nodes in the order they are input
 class OrderedGraph(nx.DiGraph):
     node_dict_factory = OrderedDict
     adjlist_dict_factory = OrderedDict
 
-cy = CyRestClient()
-cy.session.delete()
 
 class MidpointNormalize(colors.Normalize):
     def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
@@ -35,6 +38,13 @@ class MidpointNormalize(colors.Normalize):
 
 
 def f2hex_edges(fx, vmin=-0.99, vmax=0.99):
+    """
+
+    :param fx: Vector of reaction rates (flux)
+    :param vmin: Value of minimum for normalization
+    :param vmax: Value of maximum for normalization
+    :return: This function returns a vector of colors in hex format that represents flux
+    """
     norm = MidpointNormalize(vmin=vmin, vmax=vmax, midpoint=0)
     f2rgb = cm.ScalarMappable(norm=norm, cmap=sns.diverging_palette(240, 10, as_cmap=True))
     rgb = [f2rgb.to_rgba(rate)[:3] for rate in fx]
@@ -45,6 +55,14 @@ def f2hex_edges(fx, vmin=-0.99, vmax=0.99):
 
 
 def f2hex_nodes(fx, vmin, vmax, midpoint):
+    """
+
+    :param fx: Vector of species concentration
+    :param vmin: Value of minimum for normalization
+    :param vmax: Value of maximum for normalization
+    :param midpoint: Value of midpoint for normalization
+    :return: Returns a vector of colors in hex format that represents species concentration
+    """
     norm = MidpointNormalize(vmin=vmin, vmax=vmax, midpoint=midpoint)
     f2rgb = cm.ScalarMappable(norm=norm, cmap=sns.diverging_palette(150, 275, s=80, l=55, as_cmap=True))
     rgb = [f2rgb.to_rgba(rate)[:3] for rate in fx]
@@ -55,6 +73,14 @@ def f2hex_nodes(fx, vmin, vmax, midpoint):
 
 
 def r_link(graph, s, r, **attrs):
+    """
+
+    :param graph: Networkx graph
+    :param s: Source node
+    :param r: Target node
+    :param attrs: Other attributes for edge
+    :return: Links two nodes through an edge
+    """
     nodes = ('s%d' % s, 's%d' % r)
     if attrs.get('_flip'):
         del attrs['_flip']
@@ -64,28 +90,63 @@ def r_link(graph, s, r, **attrs):
 
 
 def change_node_colors(node, color, graph):
+    """
+
+    :param node: Node whose color is going to be changed
+    :param color: Color assigned to the node
+    :param graph: Networkx graph
+    :return: Updates node color
+    """
     n = graph.get_node(node)
     n.attr['fillcolor'] = color
     return
 
 
 def change_edge_colors(edge, color, graph):
+    """
+
+    :param edge: Edge whose color is going to be changed
+    :param color: Color assigned to the edge
+    :param graph: Networkx graph
+    :return: Updates edge color
+    """
     n = graph.get_edge(*edge)
     n.attr['color'] = color
     return
 
 
 def change_edge_size(edge, size, graph):
+    """
+
+    :param edge: Edge whose size is going to be changed
+    :param size: Size assigned to the edge
+    :param graph: Networkx graph
+    :return: Updates edge size
+    """
     n = graph.get_edge(*edge)
     n.attr['penwidth'] = size
     return
 
 
 def magnitude(x):
+    """
+
+    :param x: Vector of numbers
+    :return: Magnitude of numbers
+    """
     return numpy.floor(numpy.log10(x))
 
 
 def range_normalization(x, min_x, max_x, a=0.1, b=15):
+    """
+
+    :param x: Vector of numbers to be normalized
+    :param min_x: Minimum value in vector x
+    :param max_x: Maximum value in vector x
+    :param a: Value of minimum used for the normalization
+    :param b: Value of maximum used for the normalization
+    :return: Normalized vector
+    """
     return a + (x - min_x) * (b - a) / (max_x - min_x)
 
 
@@ -118,6 +179,13 @@ class FluxVisualization:
         self.edge_name2id = None
 
     def visualize(self, tspan=None, param_values=None, verbose=False):
+        """
+
+        :param tspan: time span for the simulation
+        :param param_values: Parameter values for the model
+        :param verbose: Verbose option
+        :return: Connects to cytoscape and visualize flow
+        """
         if verbose:
             print("Solving Simulation")
 
@@ -136,37 +204,24 @@ class FluxVisualization:
             # create parameter vector from the values in the model
             param_values = numpy.array([p.value for p in self.model.parameters])
 
+        # Parameters in a dictionary form
         self.param_dict = dict((p.name, param_values[i]) for i, p in enumerate(self.model.parameters))
 
-        # self.param_dict['L_0'] = 0
-        #
-        # y_pre = odesolve(self.model, self.tspan[:200], self.param_dict, verbose=verbose)
-        #
-        # self.tspan = self.tspan[201:]
-        #
-        # y_pre[-1][0] = 3000
-        # # y_pre[-1][9] = 100
-        #
-        # y0 = [y_pre['__s%d' % i][-1] for i in range(len(self.model.species))]
-        #
-        # self.y = odesolve(self.model, self.tspan, param_values=self.param_dict, y0=y0, verbose=verbose)
-
+        # Solution of the differential equations
         self.y = odesolve(self.model, self.tspan, param_values=self.param_dict)
 
         if verbose:
             print("Creating graph")
+
+        # Creates the graph from the model
         self.species_graph()
+        self.sp_graph.add_node('t', {'label': 'time', 'background-color': '#fffaf0'})
+
+        # Sets up the graph in cytoscape
         self.networkx2cytoscape_setup(self.sp_graph)
 
-        # self.sp_graph.add_node('t',
-        #                        label='time',
-        #                        shape='oval',
-        #                        fillcolor='white', style="filled", color="transparent",
-        #                        fontsize="50",
-        #                        margin="0,0",
-        #                        pos="20,20!")
-
-        self.edges_colors(self.y)
+        # Creates panda data frame with color values for edges and nodes.
+        self.edges_colors_sizes(self.y)
         self.nodes_colors(self.y)
 
         if verbose:
@@ -174,13 +229,21 @@ class FluxVisualization:
         for kx, time in enumerate(self.tspan):
             edges_size = self.size_time_edges.iloc[:, kx].to_dict()
             edges_color = self.colors_time_edges.iloc[:, kx].to_dict()
-            self.update_network(edge_color=edges_color, edge_size=edges_size)
+            time_stamp = {self.node_name2id['t']: 'time:' + ' ' + '%d' % time + ' ' + 'sec'}
+            self.update_network(edge_color=edges_color, edge_size=edges_size, time_stamp=time_stamp)
             tm.sleep(0.3)
 
-    def edges_colors(self, y):
+    def edges_colors_sizes(self, y):
+        """
+
+        :param y: Solution of the differential equations (from odesolve)
+        :return: Returns a pandas data frame where each row is an edge and each column a time point, with the color value
+        """
         all_rate_colors = {}
         all_rate_sizes = {}
         rxns_matrix = numpy.zeros((len(self.model.reactions_bidirectional), len(self.tspan)))
+
+        # Calculates matrix of reaction rates
         for idx, reac in enumerate(self.model.reactions_bidirectional):
             rate_reac = reac['rate']
             for p in self.param_dict:
@@ -191,32 +254,28 @@ class FluxVisualization:
             react_rate = func(*args)
             rxns_matrix[idx] = react_rate
 
+        # maximum an minimum reaction values at each time point
         max_all_times = [max(rxns_matrix[:, col]) for col in range(numpy.shape(rxns_matrix)[1])]
-        min_all_times = [min(rxns_matrix[:, col]) for col in range(numpy.shape(rxns_matrix)[1])]
+        # min_all_times = [min(rxns_matrix[:, col]) for col in range(numpy.shape(rxns_matrix)[1])]
 
-        max_abs_flux = numpy.array([max(i, abs(j)) for i, j in zip(max_all_times, min_all_times)])
-        max_flux_diff_area = max_abs_flux / numpy.power(numpy.array([10] * len(max_abs_flux)), magnitude(max_abs_flux))
+        # max_abs_flux = numpy.array([max(i, abs(j)) for i, j in zip(max_all_times, min_all_times)])
 
+        # TODO check global_min_rate, is it the integration error? or what is the minimum rate value that makes sense
         global_min_rate = -6
         global_max_rate = magnitude(numpy.abs(max(max_all_times)))
 
-        for rxn in self.model.reactions_bidirectional:
-            rate = rxn['rate']
-            for p in self.param_dict:
-                rate = rate.subs(p, self.param_dict[p])
-            variables = [atom for atom in rate.atoms(sympy.Symbol) if not re.match(r'\d', str(atom))]
-            func = sympy.lambdify(variables, rate, modules=dict(sqrt=numpy.lib.scimath.sqrt))
-            args = [y[str(l)] for l in variables]  # arguments to put in the lambdify function
-            react_rate = func(*args) + self.mach_eps
+        for i, rxn in enumerate(rxns_matrix):
+            react_rate = rxn + self.mach_eps
             react_rate_magnitudes = magnitude(numpy.abs(react_rate))
             react_rate_diff_area = react_rate / numpy.power(numpy.array([10] * len(react_rate)), react_rate_magnitudes)
             rate_colors = f2hex_edges(react_rate_diff_area)
             rate_sizes = range_normalization(react_rate_magnitudes, min_x=global_min_rate, max_x=global_max_rate)
-            for rctan in rxn['reactants']:
-                for pro in rxn['products']:
+            for rctan in self.model.reactions_bidirectional[i]['reactants']:
+                for pro in self.model.reactions_bidirectional[i]['products']:
                     edges_id = self.edge_name2id['s' + str(rctan) + ',s' + str(pro)]
                     all_rate_colors[edges_id] = rate_colors
                     all_rate_sizes[edges_id] = rate_sizes
+
         all_colors = pandas.DataFrame(all_rate_colors).transpose()
         all_sizes = pandas.DataFrame(all_rate_sizes).transpose()
 
@@ -224,7 +283,14 @@ class FluxVisualization:
         self.colors_time_edges = all_colors
         return
 
+    # TODO CHANGE NODE SIZE INSTEAD OF NODE COLOR TO REFLECT CHANGES IN THE CONCENTRATION
+
     def nodes_colors(self, y):
+        """
+
+        :param y: Solution of the differential equations (from odesolve)
+        :return: Returns a pandas data frame where each row is a node and each column a time point, with the color value
+        """
         all_rate_colors = {}
         initial_conditions_values = [ic[1].value for ic in self.model.initial_conditions]
         # cparp_info = curve_fit(sig_apop, self.tspan, y['cPARP'], p0=[100, 100, 100])[0]
@@ -239,6 +305,10 @@ class FluxVisualization:
         return
 
     def species_graph(self):
+        """
+
+        :return: Creates Networkx graph from PySB model
+        """
 
         graph = OrderedGraph()
         for idx, cp in enumerate(self.model.species):
@@ -262,6 +332,11 @@ class FluxVisualization:
         return self.sp_graph
 
     def networkx2cytoscape_setup(self, g):
+        """
+
+        :param g: Networkx graph
+        :return: Sets up Cytoscape graph from Networkx attributes
+        """
         pos = nx.drawing.nx_agraph.pygraphviz_layout(g, prog='dot', args="-Grankdir=LR")
         g_cy = cy.network.create_from_networkx(g)
         view_id_list = g_cy.get_views()
@@ -293,13 +368,31 @@ class FluxVisualization:
         self.view1.update_edge_views(visual_property='EDGE_TARGET_ARROW_SHAPE', values=edge_target_arrow_head)
         return
 
-    def update_network(self, edge_color, edge_size):
+    def update_network(self, edge_color, edge_size, time_stamp=None):
+        """
+
+        :param edge_color: Dictionary where the keys are SUIDs and values are the colors assigned to edges at each time point
+        :param edge_size: Dictionary where the keys are SUIDs and values are the sizes assigned to edges at each time point
+        :param time_stamp: Dictionary SUID of the node that has the time label
+        :return: Updates Cytoscape network with parameter values
+        """
         self.view1.update_edge_views(visual_property='EDGE_WIDTH', values=edge_size)
         self.view1.update_edge_views(visual_property='EDGE_STROKE_UNSELECTED_PAINT', values=edge_color)
         self.view1.update_edge_views(visual_property='EDGE_SOURCE_ARROW_UNSELECTED_PAINT', values=edge_color)
         self.view1.update_edge_views(visual_property='EDGE_TARGET_ARROW_UNSELECTED_PAINT', values=edge_color)
 
+        if time_stamp is not None:
+            self.view1.update_node_views(visual_property='NODE_LABEL', values=time_stamp)
+
 
 def run_flux_visualization(model, tspan, parameters=None, verbose=False):
+    """
+
+    :param model: PySB model to visualize
+    :param tspan: Time span of the simulation
+    :param parameters: Model parameter values
+    :param verbose: Verbose
+    :return: Returns flux visualization of the PySB model
+    """
     fv = FluxVisualization(model)
     fv.visualize(tspan, parameters, verbose)
