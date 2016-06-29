e0635be1805792eb0d6a495cfe0555bc315b102c
James Pino <james.c.pino@vanderbilt.edu>
2016-06-28 22:33:36 -0500

    Removed comments from run_tyson_oscillator
    These removes the comments as requested in  #142.

--- pysb/examples/run_tyson_oscillator.py
+++ pysb/examples/run_tyson_oscillator.py
@@ -1,15 +1,12 @@
 from pysb.examples.tyson_oscillator import model
 from pysb.integrate import odesolve
-from numpy import *
-#from pysb.tools.tropicalize import *
+import numpy as np
 import matplotlib.pyplot as plt
-# from varsens import *
 
-t = linspace(0, 100, 10001)
+t = np.linspace(0, 100, 10001)
 x = odesolve(model, t)
-ref = x
 
-#plot(t, x['CT'])  # Good validation of mass balance for cdc2, should be constant at 1
+plt.plot(t, x['CT'],  lw=2, label='YT')  # Good validation of mass balance for cdc2, should be constant at 1
 plt.plot(t, x['YT'], lw=2, label='YT')
 plt.plot(t, x['M'], lw=2, label='M')
 
@@ -18,18 +15,3 @@ plt.xlabel('time')
 plt.ylabel('population')
 
 plt.show()
-
-#m = Tropical(model)
-#m.tropicalize(t, verbose=True)
-
-#names = ['k1', 'k3', 'k4', 'kp4', 'k6', 'k7', 'k8', 'k9']
-#values = array([model.parameters[nm].value for nm in names])
-#scaling = [values-0.2*values, values+0.2*values] # 20% around values
-
-# def osc_objective(params):
-#     for i in range(len(params)):
-#         model.parameters[names[i]].value = params[i]
-#     x = odesolve(model, t)
-#     return sum((x['YT'] - ref['YT'])**2)
-
-#v = Varsens(osc_objective, len(params), 1024, scaling)
