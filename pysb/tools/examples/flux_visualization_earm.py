4201f937150168340c73d876cf0802d8bde624e3
oscar <oo.ortega2247@gmail.com>
2016-07-07 00:15:54 -0500

    updated import calls

--- pysb/tools/examples/flux_visualization_earm.py
+++ pysb/tools/examples/flux_visualization_earm.py
@@ -1,17 +1,17 @@
 from __future__ import print_function
 from earm.lopez_embedded import model
-from pysb.tools.flux_visualization import run_flux_visualization
+from pysb.tools.model_visualization import run_visualization
 import numpy as np
 import csv
 
-# tipe Bax cluster: 5400
+# type Bax cluster: 5400
 # type Bak cluster: 4052
 
-f = open('/home/oscar/Documents/tropical_project/parameters_5000/pars_embedded_5400.txt')
+f = open('/home/oscar/Documents/tropical_project/parameters_5000/pars_embedded_4052.txt')
 data = csv.reader(f)
 parames = [float(i[1]) for i in data]
 
 tspan = np.linspace(0, 20160, 100)
 
-run_flux_visualization(model, tspan, parameters=parames, verbose=False)
+run_visualization(model, tspan, parameters=parames, render_type='flux', verbose=False)
 print('finished')
--- pysb/tools/examples/render_reactions_tyson.py
+++ pysb/tools/examples/render_reactions_tyson.py
@@ -1,4 +1,7 @@
-from pysb.tools.render_models_cytoscape import run_render_model
+from pysb.tools.model_visualization import run_visualization
 from pysb.examples.tyson_oscillator import model
+# from earm.lopez_embedded import model
 
-run_render_model(model, render_type='reactions')
+run_visualization(model, render_type='reactions')
+
+# {u'NETWORK_DEPTH': 0.0, u'NETWORK_HEIGHT': 394.0, u'NETWORK_EDGE_SELECTION': True, u'NETWORK_BACKGROUND_PAINT': u'#FFFFFF', u'NETWORK_CENTER_X_LOCATION': 0.0, u'NETWORK_WIDTH': 902.0, u'NETWORK_SIZE': 550.0, u'NETWORK_CENTER_Z_LOCATION': 0.0, u'NETWORK_SCALE_FACTOR': 1.0, u'NETWORK_TITLE': u'', u'NETWORK_CENTER_Y_LOCATION': 0.0, u'NETWORK_NODE_SELECTION': True}
