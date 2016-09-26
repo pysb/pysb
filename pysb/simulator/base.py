aca4263378586283c044e03fc99dd6c6cf1c8440
Alex Lubbock <code@alexlubbock.com>
2016-09-26 15:45:15 -0500

    Allow simulation results as 3D array or list of 2D arrays; update docs

--- pysb/simulator/base.py
+++ pysb/simulator/base.py
@@ -215,15 +215,72 @@ class SimulationResult(object):
 
     Please note that the interface for this class is considered
     experimental and may change without warning as PySB is updated.
+
+    Parameters
+    ----------
+    simulator : Simulator
+        The simulator object that generated the trajectories
+    trajectories : list or numpy.ndarray
+        A set of species trajectories from a simulation. Should either be a
+        list of 2D numpy arrays or a single 3D numpy array.
+
+    Attributes
+    ----------
+    In the descriptions below, a "trajectory set" is a 2D numpy array,
+    species on first axis and time on second axis, with each element
+    containing the concentration or count of the species at the specified time.
+
+    A list of trajectory sets contains a trajectory set for each simulation.
+
+    all : list
+        List of trajectory sets. The first dimension contains species,
+        observables and expressions (in that order)
+    species : list
+        List of trajectory sets. The first dimension contains species.
+    observables : list
+        List of trajectory sets. The first dimension contains observables.
+    expressions : list
+        List of trajectory sets. The first dimension contains expressions.
+    dataframe : :py:class:`pandas.DataFrame`
+        A conversion of the trajectory sets (species, observables and
+        expressions for all simulations) into a single
+        :py:class:`pandas.DataFrame`.
     """
     def __init__(self, simulator, trajectories):
         self.squeeze = True
         self.simulator = type(simulator).__name__
         self.tout = simulator.tout
-        self._y = trajectories
-        self.nsims = len(self._y)
         self._yfull = None
         self._model = simulator._model
+
+        # Validate incoming trajectories
+        if hasattr(trajectories, 'ndim') and trajectories.ndim == 3:
+            # trajectories is a 3D array, create a list of 2D arrays
+            # This is just a view and doesn't copy the data
+            self._y = [tr for tr in trajectories]
+        else:
+            # Not a 3D array, check for a list of 2D arrays
+            try:
+                if any([tr.ndim != 2 for tr in trajectories]):
+                    raise AttributeError
+            except (AttributeError, TypeError):
+                raise ValueError("trajectories should be a 3D array or a list "
+                                 "of 2D arrays")
+            self._y = trajectories
+
+        self.nsims = len(self._y)
+        if len(self.tout) != self.nsims:
+            raise ValueError("Simulator tout should be the same length as "
+                             "trajectories")
+        for i in range(self.nsims):
+            if len(self.tout[i]) != self._y[i].shape[0]:
+                raise ValueError("The number of time points in tout[{0}] "
+                                 "should match the trajectories array for "
+                                 "simulation {0}".format(i))
+            if self._y[i].shape[1] != len(self._model.species):
+                raise ValueError("The number of species in trajectory {0} "
+                                 "should match length of "
+                                 "model.species".format(i))
 
         # Calculate ``yobs`` and ``yexpr`` based on values of ``y``
         exprs = self._model.expressions_dynamic()
--- pysb/tests/test_simulationresult.py
+++ pysb/tests/test_simulationresult.py
@@ -3,6 +3,7 @@ from pysb.simulator.base import SimulationResult
 from pysb.examples import tyson_oscillator
 import numpy as np
 
+
 def test_simres_dataframe():
     """ Test SimulationResult.dataframe() """
 
@@ -10,12 +11,31 @@ def test_simres_dataframe():
     # a time, so we fake it to test SimulationResult.dataframe()
     tspan1 = np.linspace(0, 100, 100)
     tspan2 = np.linspace(50, 100, 50)
+    tspan3 = np.linspace(100, 150, 100)
     model = tyson_oscillator.model
     sim = ScipyOdeSimulator(model, integrator='lsoda')
-    trajectories1 = sim.run(tspan=tspan1).species
+    simres1 = sim.run(tspan=tspan1)
+    # Check retrieving a single simulation dataframe
+    df_single = simres1.dataframe
+
+    # Generate multiple trajectories
+    trajectories1 = simres1.species
     trajectories2 = sim.run(tspan=tspan2).species
+    trajectories3 = sim.run(tspan=tspan3).species
+
+    # Try a simulation result with two different tspan lengths
     sim.tout = [tspan1, tspan2]
-    df = SimulationResult(sim, [trajectories1, trajectories2]).dataframe
+    simres = SimulationResult(sim, [trajectories1, trajectories2])
+    df = simres.dataframe
 
     assert df.shape == (len(tspan1) + len(tspan2),
                         len(model.species) + len(model.observables))
+
+    # Next try a simulation result with two identical tspan lengths, stacked
+    # into a single 3D array of trajectories
+    sim.tout = [tspan1, tspan3]
+    simres2 = SimulationResult(sim, np.stack([trajectories1, trajectories3]))
+    df2 = simres2.dataframe
+
+    assert df2.shape == (len(tspan1) + len(tspan3),
+                         len(model.species) + len(model.observables))
