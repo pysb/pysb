a3afda7a4b27fc9e6c700c7cb7c695aa08514301
James Pino <james.c.pino@vanderbilt.edu>
2017-02-01 14:30:16 -0600

    Moving example code into a run function.

--- pysb/examples/cupsoda/run_michment_cupsoda.py
+++ pysb/examples/cupsoda/run_michment_cupsoda.py
@@ -4,32 +4,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import itertools
 
-# factors to multiply the values of the initial conditions
-multipliers = np.linspace(0.8, 1.2, 11)
-# 2D array of initial concentrations
-initial_concentrations = [multipliers*ic[1].value for ic in model.initial_conditions]
-# Cartesian product of initial concentrations
-cartesian_product = itertools.product(*initial_concentrations)
-# the Cartesian product object must be cast to a list, then to a numpy array
-# and transposed to give a (n_species x n_vals) matrix of initial concentrations
-initials_matrix = np.array(list(cartesian_product)).T
-# we can now construct the initials dictionary
-initials = { ic[0] : initials_matrix[i] for i,ic in enumerate(model.initial_conditions) }
-# simulation time span and output points
-tspan = np.linspace(0, 50, 501)
-# run_cupsoda returns a 3D array of species and observables trajectories
-trajectories = run_cupsoda(model, tspan, initials=initials, 
-                           atol=1e-10, rtol=1e-4, verbose=True)
-# extract the trajectories for the 'Product' into a numpy array and
-# transpose to aid in plotting
-x = np.array([tr['Product'] for tr in trajectories]).T
-# plot the mean, minimum, and maximum concentrations at each time point
-plt.plot(tspan, x.mean(axis=1), 'b', lw=3, label="Product")
-plt.plot(tspan, x.max(axis=1), 'b--', lw=2, label="min/max")
-plt.plot(tspan, x.min(axis=1), 'b--', lw=2)
-# define the axis labels and legend
-plt.xlabel('time')
-plt.ylabel('concentration')
-plt.legend(loc='upper left')
-# show the plot
-plt.show()
+
+def run():
+    # factors to multiply the values of the initial conditions
+    multipliers = np.linspace(0.8, 1.2, 11)
+    # 2D array of initial concentrations
+    initial_concentrations = [multipliers*ic[1].value for ic in model.initial_conditions]
+    # Cartesian product of initial concentrations
+    cartesian_product = itertools.product(*initial_concentrations)
+    # the Cartesian product object must be cast to a list, then to a numpy array
+    # and transposed to give a (n_species x n_vals) matrix of initial concentrations
+    initials_matrix = np.array(list(cartesian_product)).T
+    # we can now construct the initials dictionary
+    initials = { ic[0] : initials_matrix[i] for i,ic in enumerate(model.initial_conditions) }
+    # simulation time span and output points
+    tspan = np.linspace(0, 50, 501)
+    # run_cupsoda returns a 3D array of species and observables trajectories
+    trajectories = run_cupsoda(model, tspan, initials=initials,
+                               atol=1e-10, rtol=1e-4, verbose=True)
+    # extract the trajectories for the 'Product' into a numpy array and
+    # transpose to aid in plotting
+    x = np.array([tr['Product'] for tr in trajectories]).T
+    # plot the mean, minimum, and maximum concentrations at each time point
+    plt.plot(tspan, x.mean(axis=1), 'b', lw=3, label="Product")
+    plt.plot(tspan, x.max(axis=1), 'b--', lw=2, label="min/max")
+    plt.plot(tspan, x.min(axis=1), 'b--', lw=2)
+    # define the axis labels and legend
+    plt.xlabel('time')
+    plt.ylabel('concentration')
+    plt.legend(loc='upper left')
+    # show the plot
+    plt.show()
+
+
+if __name__ == '__main__':
+    run()
--- pysb/examples/cupsoda/run_tyson_oscillator_cupsoda.py
+++ pysb/examples/cupsoda/run_tyson_oscillator_cupsoda.py
@@ -3,39 +3,45 @@ from pysb.simulator.cupsoda import CupSodaSimulator
 import numpy as np
 import matplotlib.pyplot as plt
 
-n_sims = 100
-vol = model.parameters['vol'].value
-tspan = np.linspace(0, 500, 501)
-sim = CupSodaSimulator(model, tspan, vol=vol, verbose=True, 
-                       integrator_options={'atol' : 1e-12, 
-                                           'rtol' : 1e-6, 
-                                           'max_steps' :20000})
 
-# Rate constants
-param_values = np.ones((n_sims, len(model.parameters)))
-for i in range(len(param_values)):
-    for j in range(len(param_values[i])):
-        param_values[i][j] *= model.parameters[j].value
+def run():
+    n_sims = 100
+    vol = model.parameters['vol'].value
+    tspan = np.linspace(0, 500, 501)
+    sim = CupSodaSimulator(model, tspan, vol=vol, verbose=True,
+                           integrator_options={'atol' : 1e-12,
+                                               'rtol' : 1e-6,
+                                               'max_steps' :20000})
 
-# Initial concentrations
-initials = np.zeros((n_sims, len(model.species)))
-for i in range(len(initials)):
-    for ic in model.initial_conditions:
-        for j in range(len(initials[i])):
-            if str(ic[0]) == str(model.species[j]):
-                initials[i][j] = ic[1].value
-                break
+    # Rate constants
+    param_values = np.ones((n_sims, len(model.parameters)))
+    for i in range(len(param_values)):
+        for j in range(len(param_values[i])):
+            param_values[i][j] *= model.parameters[j].value
 
-x = sim.run(initials=initials, param_values=param_values)
+    # Initial concentrations
+    initials = np.zeros((n_sims, len(model.species)))
+    for i in range(len(initials)):
+        for ic in model.initial_conditions:
+            for j in range(len(initials[i])):
+                if str(ic[0]) == str(model.species[j]):
+                    initials[i][j] = ic[1].value
+                    break
 
-# Plot results of the first simulation
-t = x.tout[0]
-plt.plot(t, x.all[0]['CT'], lw=2, label='CT')  # should be constant
-plt.plot(t, x.all[0]['YT'], lw=2, label='YT')
-plt.plot(t, x.all[0]['M'],  lw=2, label='M')
- 
-plt.xlabel('time')
-plt.ylabel('population')
-plt.legend(loc=0)
+    x = sim.run(initials=initials, param_values=param_values)
 
-plt.show()
+    # Plot results of the first simulation
+    t = x.tout[0]
+    plt.plot(t, x.all[0]['CT'], lw=2, label='CT')  # should be constant
+    plt.plot(t, x.all[0]['YT'], lw=2, label='YT')
+    plt.plot(t, x.all[0]['M'],  lw=2, label='M')
+
+    plt.xlabel('time')
+    plt.ylabel('population')
+    plt.legend(loc=0)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    run()
