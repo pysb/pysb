0628acda60feaae5cb4bdd4595eb289ef624665f
James Pino <james.c.pino@vanderbilt.edu>
2017-03-15 15:17:20 -0500

    Created an test for the bng simulator.
    Added support for NF simulations (as well as being able to pass a "method" to the simulator to use any simulator in bng.
    
    Adding the ability to pass param_values and initials to simulator, uses setConcentrations and setParameters (had to add to pysb/bng.py to make it work properly).

--- pysb/examples/run_bng_ssa_example.py
+++ pysb/examples/run_bng_ssa_example.py
@@ -1,15 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from pysb.simulator.bng_ssa import BioNetGenSSASimulator
+from pysb.simulator.bng_ssa import BngSimulator
 from kinase_cascade import model
+# from bax_pore import  model
 
 # We will integrate from t=0 to t=40
 t = np.linspace(0, 40, 50)
 # Simulate the model
 print("Simulating...")
-sim = BioNetGenSSASimulator(model)
-x = sim.run(tspan=t, verbose=False, n_sim=10)
+sim = BngSimulator(model, cleanup=False)
+x = sim.run(tspan=t, verbose=False, n_sim=1, cleanup=False, method='nf', gml='5024289')
 tout = x.tout
 y = np.array(x.observables)
+# plt.plot(tout.T, y['BAX4'].T)
 plt.plot(tout.T, y['ppMEK'].T)
 plt.show()
