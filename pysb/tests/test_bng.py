d9b76b2313b77f4451031cf97c9712644ac1ce42
Leonard Harris <lh64@cornell.edu>
2017-05-02 20:40:24 -0500

    Read Expression values from BNG .gdat files (#250)
    * Modified pysb.bng.BngBaseInterface.read_simulation_results() to read in
    header names from the .gdat file rather than set the names equal to the
    names of model.observables. This is necessary because BNG can be
    instructed to print function (Expression) values to the .gdat file with
    the 'print_functions=1' argument. This also required changing the 'if'
    statement from a check on whether model.observables exists to whether
    the .gdat file exists (since, in principle, the .gdat file could contain
    only functions).
    
    * Add NFsim simulation result support
    
    NFsim generates a .gdat (groups, i.e. observables/expressions) but not a
    .cdat (species concentrations) file. I've updated read_simulation_results in
    the BioNetGen interface to take into account simulation results may create
    .cdat, .gdat, or both.
    
    I've also made the .gdat file reader only open the file once for efficiency,
    and added an NFsim unit test (Simulator class support coming soon).

--- pysb/tests/test_bng.py
+++ pysb/tests/test_bng.py
@@ -112,6 +112,37 @@ def test_zero_order_synth_no_initials():
 
 
 @with_model
+def test_nfsim():
+    Monomer('A', ['a'])
+    Monomer('B', ['b'])
+
+    Parameter('ksynthA', 100)
+    Parameter('ksynthB', 100)
+    Parameter('kbindAB', 100)
+
+    Parameter('A_init', 20)
+    Parameter('B_init', 30)
+
+    Initial(A(a=None), A_init)
+    Initial(B(b=None), B_init)
+
+    Observable("A_free", A(a=None))
+    Observable("B_free", B(b=None))
+    Observable("AB_complex", A(a=1) % B(b=1))
+
+    Rule('A_synth', None >> A(a=None), ksynthA)
+    Rule('B_synth', None >> B(b=None), ksynthB)
+    Rule('AB_bind', A(a=None) + B(b=None) >> A(a=1) % B(b=1), kbindAB)
+
+    with BngFileInterface(model) as bng:
+        bng.action('simulate', method='nf', t_end=1000, n_steps=100)
+        bng.execute()
+        res = bng.read_simulation_results()
+        assert res.dtype.names == ('time', 'A_free', 'B_free', 'AB_complex')
+        assert len(res) == 101
+
+
+@with_model
 def test_unicode_strs():
     Monomer(u'A', [u'b'], {u'b':[u'y', u'n']})
     Monomer(u'B')
