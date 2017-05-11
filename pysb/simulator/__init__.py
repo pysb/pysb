6f34df79a0809f680fbd6e6c25b25cefa09a00f2
Alex Lubbock <code@alexlubbock.com>
2017-05-11 16:58:49 +0200

    GPU SSA

--- pysb/simulator/__init__.py
+++ pysb/simulator/__init__.py
@@ -3,6 +3,8 @@ from .scipyode import ScipyOdeSimulator
 from .cupsoda import CupSodaSimulator
 from .stochkit import StochKitSimulator
 from .bng import BngSimulator, PopulationMap
+from .gpu_ssa import GPUSimulator
 
 __all__ = ['BngSimulator', 'CupSodaSimulator', 'ScipyOdeSimulator',
-           'StochKitSimulator', 'SimulationResult', 'PopulationMap']
+           'StochKitSimulator', 'SimulationResult', 'PopulationMap',
+           'GPUSimulator']
