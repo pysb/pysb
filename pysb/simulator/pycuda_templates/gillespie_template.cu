2609d8b1168d807b5c70b219051765acf09d0a67
James Pino <james.c.pino@vanderbilt.edu>
2017-05-17 16:32:23 +0200

    Updated GPU ssa to use local memory for parameter rates, rather than the old out dated paramtexture.

--- pysb/simulator/pycuda_templates/gillespie_template.cu
+++ pysb/simulator/pycuda_templates/gillespie_template.cu
@@ -1,6 +1,5 @@
 #include <curand_kernel.h>
 #include <stdio.h>
-texture<float, 2, cudaReadModeElementType> param_tex;
 #define num_species {n_species}
 #define NPARAM {n_params}
 #define NREACT {n_reactions}
@@ -10,7 +9,7 @@ __constant__ int stoch_matrix[]={{
 }};
 
 
-__device__ void propensities(int *y, double *h, int tid)
+__device__ void propensities(int *y, double *h, int tid, double *param_arry)
 {{
 {hazards}
 }}
@@ -31,8 +30,8 @@ __device__ int sample(int nr, double* a, float u){{
     return i;
 }}
 ///*
-__global__ void Gillespie_all_steps(int* species_matrix, int* result, float* time,
-                                    int NRESULTS){{
+__global__ void Gillespie_all_steps(int* species_matrix, int* result, double* time, int NRESULTS,
+                                    double* param_values){{
 
     int tid = blockDim.x * blockIdx.x + threadIdx.x;
     //curandState randState;
@@ -45,6 +44,11 @@ __global__ void Gillespie_all_steps(int* species_matrix, int* result, float* tim
     int k = 0;
     int y[num_species];
     double A[NREACT];
+    double param_arry[NPARAM];
+
+    for(int i=0; i<NPARAM; i++){{
+        param_arry[i] = param_values[tid*NPARAM + i];
+        }}
 
     // start first step
     for(int i=0; i<NREACT; i++){{
@@ -55,7 +59,7 @@ __global__ void Gillespie_all_steps(int* species_matrix, int* result, float* tim
         y[i] = species_matrix[i];
         }}
 
-    propensities( y, A , tid);
+    propensities( y, A , tid, param_arry);
 
     for(int i=0; i<NREACT; i++){{
         a0 += A[i];
@@ -73,7 +77,7 @@ __global__ void Gillespie_all_steps(int* species_matrix, int* result, float* tim
             r1 =  curand_uniform(&randState);
             k = sample(NREACT, A, a0*r1);
             stoichiometry( y ,k );
-            propensities( y, A, tid );
+            propensities( y, A, tid , param_arry);
             a0 = 0;
             // summing up propensities
             for(int j=0; j<NREACT; j++)
@@ -86,14 +90,15 @@ __global__ void Gillespie_all_steps(int* species_matrix, int* result, float* tim
     return;
     }}
 //*/
-__global__ void Gillespie_one_step(int* species_matrix, int* result, float* start_time,
-                                    float end_time, float* result_time, int stride){{
+
+__global__ void Gillespie_one_step(int* species_matrix, int* result, double* start_time, double end_time,
+                                   double* result_time, int stride, double* param_values){{
 
     int tid = blockDim.x * blockIdx.x + threadIdx.x;
     //curandState randState;
     curandStateMRG32k3a randState;
     curand_init(clock64(), tid, 0, &randState);
-    float start_t = start_time[tid] ;
+    double start_t = start_time[tid] ;
 
     double r1 = 0.0f;
     double r2 = 0.0f;
@@ -103,6 +108,11 @@ __global__ void Gillespie_one_step(int* species_matrix, int* result, float* star
     int y[num_species];
     int ylast[num_species];
 
+    double param_arry[NPARAM];
+
+    for(int i=0; i<NPARAM; i++){{
+        param_arry[i] = param_values[tid*NPARAM + i];
+        }}
     double A[NREACT];
 
     // start first step
@@ -114,7 +124,7 @@ __global__ void Gillespie_one_step(int* species_matrix, int* result, float* star
         y[i] = ((int*)( (char*) species_matrix + tid * stride))[i];
         }}
 
-    propensities( y, A , tid);
+    propensities( y, A, tid , param_arry);
 
     for(int i=0; i<NREACT; i++){{
         a0 += A[i];
@@ -125,29 +135,25 @@ __global__ void Gillespie_one_step(int* species_matrix, int* result, float* star
         r1 =  curand_uniform(&randState);
         k = sample(NREACT, A, a0*r1);
         stoichiometry( y ,k );
-        propensities( y, A, tid );
+        propensities( y, A, tid , param_arry);
         a0 = 0;
         // summing up propensities
         for(int j=0; j<NREACT; j++){{
             a0 += A[j];
             }}
 
-//        __syncthreads();
 
         r2 =  curand_uniform(&randState);
         dt = -__logf(r2)/a0;
         start_t += dt;
-        for(int j=0; j<num_species; j++){{
-            ylast[j] = y[j];
-            }}
+
         }}
 
     for(int j=0; j<num_species; j++){{
-//        result[tid*num_species +  j] = y[j];
-        result[tid*num_species +  j] = ylast[j];
+        result[tid*num_species +  j] = y[j];
         }}
-//    start_t -= dt;
     result_time[tid] = start_t;
     return;
     }}
+
 }}
