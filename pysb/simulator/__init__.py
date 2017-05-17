878bd348bccafffa0b499b006ce934001b2f0ca9
Alex Lubbock <code@alexlubbock.com>
2017-05-17 17:38:53 +0200

    Add BngSimulator to pysb.simulator namespace

--- pysb/simulator/__init__.py
+++ pysb/simulator/__init__.py
@@ -2,6 +2,7 @@ from .base import SimulatorException, SimulationResult
 from .scipyode import ScipyOdeSimulator
 from .cupsoda import CupSodaSimulator
 from .stochkit import StochKitSimulator
+from .bng import BngSimulator
 
-__all__ = ['CupSodaSimulator', 'ScipyOdeSimulator', 'StochKitSimulator',
-           'SimulationResult']
+__all__ = ['BngSimulator', 'CupSodaSimulator', 'ScipyOdeSimulator',
+           'StochKitSimulator', 'SimulationResult']
