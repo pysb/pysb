ffbbfd40e7d7755626813d62db2f096bc2069da3
James Pino <james.c.pino@vanderbilt.edu>
2017-05-18 14:09:51 +0200

    Reverting schogl to original version, simplifed where constant species is taken into account in the parameter rather than the species.

--- pysb/examples/schlogl.py
+++ pysb/examples/schlogl.py
@@ -1,29 +1,38 @@
 from pysb import *
 
 Model()
-Parameter('atol', 1e5)
-Parameter('Btot', 2e5)
-Parameter('k1', 3e-7)
-Parameter('k2', 1e-4)
-Parameter('k2_n', 1e-4/3)
-Parameter('k3', 1e-3)
-Parameter('k4', 3.5)
-Parameter('atol_k1', 1e5 * 3e-7)
-Parameter('btol_k3', 2e5 * 1e-3)
 Monomer('X')
 Monomer('A')
 Monomer('B')
+
+Parameter('X_0', 100)
+Parameter('A_0', 1e5)
+Parameter('B_0', 2e5)
+
+Initial(X(), X_0)
+Initial(A(), A_0)
+Initial(B(), B_0)
+
+Parameter('k1', 3e-7)
+# Parameter('k2', 1e-4)
+
+Parameter('k3', 1e-3)
+Parameter('k4', 3.5)
+
+Parameter('k2_n', 1e-4 / 3.)
+
+Parameter('atol_k1', A_0.value * k1.value)
+Parameter('btol_k3', B_0.value * k3.value)
+
+# Rule('rule1', A() + X() + X() <> A() + X() + X() + X(), k1, k2_n)
+# Rule('rule2', B() <> X() + B(), k3, k4)
+
 Monomer('I')
-Initial(X(), Parameter('X_0', 100))
-Initial(A(), Parameter('A_0', 1e5))
-Initial(B(), Parameter('B_0', 1e5))
 Initial(I(), Parameter('I_0', 1))
+Rule('rule3', X() + X() <> X() + X() + X(), atol_k1, k2_n)
+Rule('rule4', I() <> X() + I(), btol_k3, k4)
 
-Rule('rule1', A() + X() + X() <> A() + X() + X() + X(), k1, k2)
-Rule('rule2', B() <> X() + B(), k3, k4)
 
-# Rule('rule3', X() + X() <> X() + X() + X(), atol_k1, k2_n)
-# Rule('rule4', I() <> X() + I() , btol_k3, k4)
 Observable('X_total', X())
-Observable('A_total', A())
-Observable('B_total', B())
+# Observable('A_total', A())
+# Observable('B_total', B())
