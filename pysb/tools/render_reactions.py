184e7e43ddd9a52b37010d7bd0d2ade202bde165
Alex Lubbock <code@alexlubbock.com>
2018-01-16 21:14:29 -0600

    networkx/pydot support for reading .dot files
    This PR makes pygraphviz support optional and introduces pydot as an
    alternative for reading .dot (graphviz) files. pygraphviz support on
    Windows on recent versions of Python is patchy at best. pydot is
    easier to install and provides an alternative.
    
    When dot file reading is required (kappa interface), the results are
    now returned in the (more flexible and mainted) networkx format.
    The dot file itself can be read using pydot or pygraphviz (both are
    tried by default) - see new pysb.util.read_dot() function.
    
    This change also allows us to move appveyor onto Python 3.6 -
    pygraphviz was the hurdle that was previously preventing this.

--- pysb/tools/render_reactions.py
+++ pysb/tools/render_reactions.py
@@ -54,11 +54,14 @@ as a "modifier" (enzyme or catalyst).
 from __future__ import print_function
 import pysb
 import pysb.bng
-import sympy
 import re
 import sys
 import os
-import pygraphviz
+try:
+    import pygraphviz
+except ImportError:
+    pygraphviz = None
+
 
 def run(model):
     """
@@ -74,6 +77,9 @@ def run(model):
     string
         The dot format output.
     """
+    if pygraphviz is None:
+        raise ImportError('pygraphviz library is required to run this '
+                          'function')
 
     pysb.bng.generate_equations(model)
 
