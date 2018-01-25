99c92f9dfd3d6ec1ce1370dc788632af633e9a8b
Alex Lubbock <code@alexlubbock.com>
2018-01-25 09:44:24 -0600

    Remove Nones from kappa unicode test rxn pat

--- pysb/tests/test_kappa.py
+++ pysb/tests/test_kappa.py
@@ -197,11 +197,10 @@ def test_influence_map_kasa():
 @with_model
 def test_unicode_strs():
     Monomer(u'A', [u'b'], {u'b':[u'y', u'n']})
-    Monomer(u'B')
-    Rule(u'rule1', A(b=u'y') + None >> None + B(),
+    Rule(u'rule1', A(b=u'y') >> A(b=u'n'),
          Parameter(u'k', 1))
     Initial(A(b=u'y'), Parameter(u'A_0', 100))
-    Observable(u'B_', B())
+    Observable(u'A_y', A(b=u'y'))
     npts = 200
     kres = run_simulation(model, time=100, points=npts, seed=_KAPPA_SEED)
 
