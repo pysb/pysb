5c17fd76af2e4f5782718c9c9312ed4869b0b137
Alex Lubbock <code@alexlubbock.com>
2018-07-24 15:11:14 -0500

    Unit test for SBML export "observables"

--- pysb/tests/test_exporters.py
+++ pysb/tests/test_exporters.py
@@ -54,15 +54,20 @@ def check_convert(model, format):
                 raise SkipTest("SBML Simulation test skipped (requires roadrunner)")
 
             roadrunner.Logger.setLevel(roadrunner.Logger.LOG_ERROR)
+
             # Simulate SBML using roadrunner
             rr = roadrunner.RoadRunner(exported_file)
+            rr.timeCourseSelections = \
+                ['__s{}'.format(i) for i in range(len(model.species))] + \
+                ['__obs{}'.format(i) for i in range(len(model.observables))]
             rr_result = rr.simulate(0, 10, 100)
 
             # Simulate original using PySB
             df = ScipyOdeSimulator(model).run(tspan=np.linspace(0, 10, 100)).dataframe
 
+            # Compare species' trajectories
             for sp_idx in range(len(model.species)):
-                rr_sp = rr_result[:, sp_idx + 1]
+                rr_sp = rr_result[:, sp_idx]
                 py_sp = df.iloc[:, sp_idx]
                 is_close = np.allclose(rr_sp, py_sp, rtol=1e-4)
                 if not is_close:
@@ -70,3 +75,12 @@ def check_convert(model, format):
                     raise ValueError('Model {}, species __s{} trajectories do not match:'.format(
                         model.name, sp_idx))
 
+            # Compare observables' trajectories
+            for obs_idx in range(len(model.observables)):
+                rr_obs = rr_result[:, obs_idx + len(model.species)]
+                py_obs = df.iloc[:, obs_idx + len(model.species)]
+                is_close = np.allclose(rr_obs, py_obs, rtol=1e-4)
+                if not is_close:
+                    print(pd.DataFrame(dict(rr=rr_obs, pysb=py_obs)))
+                    raise ValueError('Model {}, observable__o{} "{}" trajectories do not match:'.format(
+                        model.name, obs_idx, model.observables[obs_idx].name))
