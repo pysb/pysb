e3302ac22cdbab9109b4b614b0547f6d3a793ae8
Alex Lubbock <alex@bismo.dhcp.mc.vanderbilt.edu>
2018-11-29 18:42:38 -0600

    Fix robertson and tutorial_a example models

--- pysb/examples/run_tutorial_a.py
+++ pysb/examples/run_tutorial_a.py
@@ -5,4 +5,4 @@ from tutorial_a import model
 t = [0, 10, 20, 30, 40, 50, 60]
 simulator = ScipyOdeSimulator(model, tspan=t)
 simresult = simulator.run()
-print(simresult.species[:, 1])
+print(simresult.species)
