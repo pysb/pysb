702ee380b45320df9bf01eece8d094c4fedf3b3c
Alex Lubbock <code@alexlubbock.com>
2019-01-09 15:47:54 -0600

    Cython compiler directives for faster simulations
    I created a PR for Cython (https://github.com/cython/cython/pull/2595)
    which added compiler directive support for cython.inline(), which
    is now present in Cython>=0.92
    
    This PR introduces user-configurable Cython compiler directives
    within the ScipyOdeSimulator. By default, these directives disable
    bounds, wrap around, "None", and initialized checks. Some initial
    benchmarking on asv shows a speed improvement of about 5% on my
    laptop (every little helps?)

--- benchmarks/simulator.py
+++ benchmarks/simulator.py
@@ -14,23 +14,34 @@ class Earm10ODESuite(object):
             [[p.value for p in self.model.parameters]], self.nsims, axis=0)
         integrator_options_common = {
             'model': self.model,
-            'tspan': np.linspace(0, 1000, 101),
-            'atol': 1e-6,
-            'rtol': 1e-6,
-            'mxsteps': 20000,
+            'tspan': np.linspace(0, 20000, 101),
             'param_values': self.parameter_set
         }
 
         self.sim_lsoda = ScipyOdeSimulator(
             integrator='lsoda',
+            compiler='cython',
+            integrator_options={'atol': 1e-6, 'rtol': 1e-6, 'mxstep': 20000},
             **integrator_options_common
         )
+        self.sim_lsoda_no_compiler_directives = ScipyOdeSimulator(
+            integrator='lsoda',
+            compiler='cython',
+            cython_directives={},
+            integrator_options={'atol': 1e-6, 'rtol': 1e-6, 'mxstep': 20000},
+            **integrator_options_common
+        )
+
         self.sim_cupsoda = CupSodaSimulator(
+            integrator_options={'atol': 1e-6, 'rtol': 1e-6, 'max_steps': 20000},
             **integrator_options_common
         )
 
     def time_scipy_lsoda(self):
         self.sim_lsoda.run()
+
+    def time_scipy_lsoda_no_compiler_directives(self):
+        self.sim_lsoda_no_compiler_directives.run()
 
     def time_cupsoda(self):
         self.sim_cupsoda.run()
