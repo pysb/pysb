c859bad5c8f2ec08b01e7327f74b1086fcea623b
Alex Lubbock <code@alexlubbock.com>
2019-02-01 17:12:08 -0600

    CupSodaSimulator multi-GPU support

--- pysb/simulator/cupsoda.py
+++ pysb/simulator/cupsoda.py
@@ -14,6 +14,7 @@ from pysb.logging import EXTENDED_DEBUG
 import shutil
 from pysb.pathfinder import get_path
 import sympy
+import collections
 
 try:
     import pandas as pd
@@ -92,8 +93,8 @@ class CupSodaSimulator(Simulator):
     verbose: bool or int
         Verbosity setting. See the base class
         :class:`pysb.simulator.base.Simulator` for further details.
-    gpu : int
-        Index of GPU being run on
+    gpu : int or list
+        Index of GPU being run on, or a list of integers to use multiple GPUs
     vol : float or None
         System volume
     n_blocks: int
@@ -250,58 +251,90 @@ class CupSodaSimulator(Simulator):
                                           param_values=param_values,
                                           _run_kwargs=[])
 
-        # Create directories for cupSODA input and output files
-        self.outdir = tempfile.mkdtemp(prefix=self._prefix + '_',
-                                       dir=self._base_dir)
+        if isinstance(self.gpu, collections.Iterable):
+            gpus = self.gpu
+        else:
+            gpus = [self.gpu]
 
-        self._logger.debug("Output directory is %s" % self.outdir)
-        _cupsoda_infiles_dir = os.path.join(self.outdir, "INPUT")
-        os.mkdir(_cupsoda_infiles_dir)
-        self._cupsoda_outfiles_dir = os.path.join(self.outdir, "OUTPUT")
+        # Create directories for cupSODA input and output files
+        _outdirs = {}
+        _indirs = {}
+        p = {}
+
+        # Split simulations equally between GPUs
+        sims = dict(zip(gpus, np.array_split(range(len(self.param_values)),
+                                             len(gpus))))
 
         # Path to cupSODA executable
         bin_path = get_path('cupsoda')
 
-        # Create cupSODA input files
-        self._create_input_files(_cupsoda_infiles_dir)
-
-        # Build command
-        # ./cupSODA input_model_folder blocks output_folder simulation_
-        # file_prefix gpu_number fitness_calculation memory_use dump
-        command = [bin_path, _cupsoda_infiles_dir, str(self.n_blocks),
-                   self._cupsoda_outfiles_dir, self._prefix, str(self.gpu),
-                   '0', self._memory_usage, str(self._cupsoda_verbose)]
-
-        self._logger.info("Running cupSODA: " + ' '.join(command))
         start_time = time.time()
-        # Run simulation and return trajectories
-        p = subprocess.Popen(command, stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE)
-        # for line in iter(p.stdout.readline, b''):
-        #     if 'Running time' in line:
-        #         self._logger.info(line[:-1])
-        (p_out, p_err) = p.communicate()
-        p_out = p_out.decode('utf-8')
-        p_err = p_err.decode('utf-8')
-        logger_level = self._logger.logger.getEffectiveLevel()
-        if logger_level <= logging.INFO:
-            run_time_match = self._running_time_regex.search(p_out)
-            if run_time_match:
-                self._logger.info('cupSODA reported time: {} '
-                                  'seconds'.format(run_time_match.group(1)))
-        self._logger.debug('cupSODA stdout:\n' + p_out)
-        if p_err:
-            self._logger.error('cupsoda strerr:\n' + p_err)
-        if p.returncode:
-            raise SimulatorException(
-                p_out.rstrip("at line") + "\n" + p_err.rstrip())
-        tout, trajectories = self._load_trajectories(
-            self._cupsoda_outfiles_dir)
+
+        cmtx = self._get_cmatrix()
+
+        outdir = tempfile.mkdtemp(prefix=self._prefix + '_',
+                                  dir=self._base_dir)
+        self._logger.debug("Output directory is %s" % outdir)
+
+        for gpu in gpus:
+            _indirs[gpu] = os.path.join(outdir, "INPUT" + str(gpu))
+            os.mkdir(_indirs[gpu])
+            _outdirs[gpu] = os.path.join(outdir, "OUTPUT" + str(gpu))
+
+            # Create cupSODA input files
+            self._create_input_files(_indirs[gpu], sims[gpu], cmtx)
+
+            # Build command
+            # ./cupSODA input_model_folder blocks output_folder simulation_
+            # file_prefix gpu_number fitness_calculation memory_use dump
+            command = [bin_path, _indirs[gpu], str(self.n_blocks),
+                       _outdirs[gpu], self._prefix, str(gpu),
+                       '0', self._memory_usage, str(self._cupsoda_verbose)]
+
+            self._logger.info("Running cupSODA: " + ' '.join(command))
+
+            # Run simulation and return trajectories
+            p[gpu] = subprocess.Popen(command, stdout=subprocess.PIPE,
+                                      stderr=subprocess.PIPE)
+
+        tout = None
+        trajectories = None
+        for gpu in gpus:
+            (p_out, p_err) = p[gpu].communicate()
+            p_out = p_out.decode('utf-8')
+            p_err = p_err.decode('utf-8')
+            logger_level = self._logger.logger.getEffectiveLevel()
+            if logger_level <= logging.INFO:
+                run_time_match = self._running_time_regex.search(p_out)
+                if run_time_match:
+                    self._logger.info('cupSODA GPU {} reported time: {} '
+                                      'seconds'.format(
+                        gpu,
+                        run_time_match.group(1)))
+            self._logger.debug('cupSODA GPU {} stdout:\n{}'.format(gpu, p_out))
+            if p_err:
+                self._logger.error('cupSODA GPU {} strerr:\n{}'.format(gpu, p_err))
+            if p[gpu].returncode:
+                raise SimulatorException(
+                    "cupSODA GPU {} exception:\n{}\n{}".format(
+                        gpu, p_out.rstip("at line"), p_err.rstrip()
+                    )
+                )
+            tout_run, trajectories_run = self._load_trajectories(
+                _outdirs[gpu], sims[gpu])
+            if trajectories is None:
+                tout = tout_run
+                trajectories = trajectories_run
+            else:
+                tout = np.concatenate((tout, tout_run))
+                trajectories = np.concatenate((trajectories, trajectories_run))
+
         if self._cleanup:
-            shutil.rmtree(self.outdir)
+            shutil.rmtree(outdir)
+
         end_time = time.time()
-        self._logger.info("cupSODA + I/O time: {} seconds".format(end_time -
-                                                                  start_time))
+        self._logger.info("cupSODA + I/O time: {} seconds".format(
+            end_time - start_time))
         return SimulationResult(self, tout, trajectories)
 
     @property
@@ -359,10 +392,7 @@ class CupSodaSimulator(Simulator):
             raise ValueError("n_blocks must be greater than 0")
         self.opts['n_blocks'] = n_blocks
 
-    def _create_input_files(self, directory):
-
-        n_sims = len(self.param_values)
-
+    def _create_input_files(self, directory, sims, cmtx):
         # atol_vector
         with open(os.path.join(directory, "atol_vector"), 'w') as atol_vector:
             for i in range(self._len_species):
@@ -372,15 +402,14 @@ class CupSodaSimulator(Simulator):
 
         # c_matrix
         with open(os.path.join(directory, "c_matrix"), 'w') as c_matrix:
-            cmtx = self._get_cmatrix()
-            for i in range(n_sims):
+            for i in sims:
                 line = ""
                 for j in range(self._len_rxns):
                     if j > 0:
                         line += "\t"
                     line += str(cmtx[i][j])
                 c_matrix.write(line)
-                if i < n_sims - 1:
+                if i != sims[-1]:
                     c_matrix.write("\n")
 
         # cs_vector
@@ -434,14 +463,14 @@ class CupSodaSimulator(Simulator):
             if self.vol:
                 mx0 = mx0.copy()
                 mx0 /= (N_A * self.vol)
-            for i in range(n_sims):
+            for i in sims:
                 line = ""
                 for j in range(self._len_species):
                     if j > 0:
                         line += "\t"
                     line += str(mx0[i][j])
                 MX_0.write(line)
-                if i < n_sims - 1:
+                if i != sims[-1]:
                     MX_0.write("\n")
 
         # right_side
@@ -511,7 +540,7 @@ class CupSodaSimulator(Simulator):
         self._logger.debug("100%")
         return c_matrix
 
-    def _load_trajectories(self, directory):
+    def _load_trajectories(self, directory, sims):
         """Read simulation results from output files.
 
         Returns `tout` and `trajectories` arrays.
@@ -521,11 +550,11 @@ class CupSodaSimulator(Simulator):
         if len(files) == 0:
             raise SimulatorException(
                 "Cannot find any output files to load data from.")
-        if len(files) != len(self.param_values):
+        if len(files) != len(sims):
             raise SimulatorException(
-                "Number of input files (%d) does not match number "
+                "Number of output files (%d) does not match number "
                 "of requested simulations (%d)." % (
-                len(files), len(self.param_values)))
+                len(files), len(sims)))
         n_sims = len(files)
         trajectories = [None] * n_sims
         tout = [None] * n_sims
@@ -533,14 +562,14 @@ class CupSodaSimulator(Simulator):
         tout_n = np.ones(len(self.tspan)) * float('nan')
         # load the data
         indir_prefix = os.path.join(directory, self._prefix)
-        for n in range(n_sims):
-            trajectories[n] = traj_n.copy()
-            tout[n] = tout_n.copy()
-            filename = indir_prefix + "_" + str(n)
+        for idx, n in enumerate(sims):
+            trajectories[idx] = traj_n.copy()
+            tout[idx] = tout_n.copy()
+            filename = indir_prefix + "_" + str(idx)
             if not os.path.isfile(filename):
                 raise Exception("Cannot find input file " + filename)
             # determine optimal loading method
-            if n == 0:
+            if idx == 0:
                 (data, use_pandas) = self._test_pandas(filename)
             # load data
             else:
@@ -549,11 +578,11 @@ class CupSodaSimulator(Simulator):
                 else:
                     data = self._load_with_openfile(filename)
             # store data
-            tout[n] = data[:, 0]
-            trajectories[n][:, self._out_species] = data[:, 1:]
+            tout[idx] = data[:, 0]
+            trajectories[idx][:, self._out_species] = data[:, 1:]
             # volume correction
             if self.vol:
-                trajectories[n][:, self._out_species] *= (N_A * self.vol)
+                trajectories[idx][:, self._out_species] *= (N_A * self.vol)
         return np.array(tout), np.array(trajectories)
 
     def _test_pandas(self, filename):
