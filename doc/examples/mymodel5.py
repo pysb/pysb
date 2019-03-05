b3c18a84e2384654062f8a9480ed7cfe2a095656
Alex Lubbock <code@alexlubbock.com>
2019-03-05 13:10:39 -0600

    Tutorial fixes
    * Fix incorrect state (#421)
    * Update figure (wrong x-axis)
    * Fix observable trajectory
    * Correct some indentation
    
    Fixes: #421

--- doc/examples/mymodel5.py
+++ doc/examples/mymodel5.py
@@ -4,13 +4,13 @@ from pysb import *
 
 def catalyze(enz, sub, site, state1, state2, kf, kr, kc):   # function call
     """2-step catalytic process"""                          # reaction name
-    r1_name = '%s_assoc_%s' % (enz.name, sub.name)           # name of association reaction for rule
+    r1_name = '%s_assoc_%s' % (enz.name, sub.name)          # name of association reaction for rule
     r2_name = '%s_diss_%s' % (enz.name, sub.name)           # name of dissociation reaction for rule
     E = enz(b=None)                                         # define enzyme state in function
     S = sub({'b': None, site: state1})                      # define substrate state in function
     ES = enz(b=1) % sub({'b': 1, site: state1})             # define state of enzyme:substrate complex
     P = sub({'b': None, site: state2})                      # define state of product
-    Rule(r1_name, E + S | ES, kf, kr)                      # rule for enzyme + substrate association (bidirectional)
+    Rule(r1_name, E + S | ES, kf, kr)                       # rule for enzyme + substrate association (bidirectional)
     Rule(r2_name, ES >> E + P, kc)                          # rule for enzyme:substrate dissociation  (unidirectional)
    
 # instantiate a model
@@ -26,7 +26,7 @@ Parameter('kr', 1.0e-03)
 Parameter('kc', 1.0)
 
 # OLD RULES
-# Rule('C8_Bid_bind', C8(b=None) + Bid(b=None, S=None) | C8(b=1) % Bid(b=1, S=None), *[kf, kr])
+# Rule('C8_Bid_bind', C8(b=None) + Bid(b=None, S='u') | C8(b=1) % Bid(b=1, S='u'), *[kf, kr])
 # Rule('tBid_from_C8Bid', C8(b=1) % Bid(b=1, S='u') >> C8(b=None) + Bid(b=None, S='t'), kc)
 #
 # NEW RULES
