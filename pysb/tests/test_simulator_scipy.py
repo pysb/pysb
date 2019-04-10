2362d2bb3f233dac40228e882df2600b1ac05658
Alex Lubbock <code@alexlubbock.com>
2019-04-10 12:22:10 -0500

    Use generator for Scipy simulator parallel tests

--- pysb/tests/test_simulator_scipy.py
+++ pysb/tests/test_simulator_scipy.py
@@ -358,45 +358,21 @@ class TestScipySimulatorMultiple(TestScipySimulatorBase):
 
     @unittest.skipIf(sys.version_info.major < 3,
                      'Parallel execution requires Python >= 3.3')
-    def test_parallel_vode(self):
-        initials = [[10, 20, 30], [50, 60, 70]]
-        base_res = self.sim.run(initials=initials)
-        res = self.sim.run(initials=initials, num_processors=2)
-        assert np.allclose(res.species, base_res.species)
+    def test_parallel(self):
+        for integrator in ('vode', 'lsoda'):
+            for use_analytic_jacobian in (True, False):
+                yield self._check_parallel, integrator, use_analytic_jacobian
 
-    @unittest.skipIf(sys.version_info.major < 3,
-                     'Parallel execution requires Python >= 3.3')
-    def test_parallel_vode_jacobian(self):
+    def _check_parallel(self, integrator, use_analytic_jacobian):
         initials = [[10, 20, 30], [50, 60, 70]]
-        sim = ScipyOdeSimulator(self.model, self.sim.tspan,
-                                 initials=initials,
-                                 use_analytic_jacobian=True)
-        base_res = sim.run()
-        res = sim.run(num_processors=2)
-        assert np.allclose(res.species, base_res.species)
-
-
-    @unittest.skipIf(sys.version_info.major < 3,
-                     'Parallel execution requires Python >= 3.3')
-    def test_parallel_lsoda(self):
-        initials = [[10, 20, 30], [50, 60, 70]]
-        sim = ScipyOdeSimulator(self.model, self.sim.tspan,
-                                 initials=initials,
-                                 integrator='lsoda')
-        base_res = sim.run()
-        res = sim.run(num_processors=2)
-        assert np.allclose(res.species, base_res.species)
-
-    @unittest.skipIf(sys.version_info.major < 3,
-                     'Parallel execution requires Python >= 3.3')
-    def test_parallel_lsoda_jacobian(self):
-        initials = [[10, 20, 30], [50, 60, 70]]
-        sim = ScipyOdeSimulator(self.model, self.sim.tspan,
-                                 integrator='lsoda',
-                                 initials=initials,
-                                 use_analytic_jacobian=True)
-        base_res = sim.run()
-        res = sim.run(num_processors=2)
+        sim = ScipyOdeSimulator(
+            self.model, self.sim.tspan,
+            initials=initials,
+            integrator=integrator,
+            use_analytic_jacobian=use_analytic_jacobian
+        )
+        base_res = sim.run(initials=initials)
+        res = sim.run(initials=initials, num_processors=2)
         assert np.allclose(res.species, base_res.species)
 
 
