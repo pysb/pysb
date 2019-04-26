2043ebef923baa039d3de1be0a7de35af8d27dde
Alex Lubbock <code@alexlubbock.com>
2019-04-26 13:58:43 -0500

    KappaSimulator
    A simulation engine for Kappa built on the PySB Simulator class.
    Supports multiple realizations, and multiple initial condition
    and parameter sets.

---
+++ pysb/tests/test_simulator_kappa.py
@@ -0,0 +1,48 @@
+from pysb.simulator import KappaSimulator
+from pysb.kappa import run_simulation
+from pysb.examples import michment
+import numpy as np
+
+
+KAPPA_SEED = 123
+
+
+def _compare_kappa_sims(run_simulation_output, kappasimulator_output):
+    sim1 = run_simulation_output.view('<f8')[:, 1:]
+    sim2 = kappasimulator_output.observables.view('f8').reshape(sim1.shape)
+
+    assert np.allclose(sim1, sim2, atol=1e-16, rtol=1e-16)
+
+
+def test_kappa_sim_michment():
+    orig_sim = run_simulation(michment.model, time=100, points=100, seed=123)
+
+    sim = KappaSimulator(michment.model, tspan=np.linspace(0, 100, 101))
+    x = sim.run(seed=KAPPA_SEED)
+
+    _compare_kappa_sims(orig_sim, x)
+
+
+def test_kappa_2sims():
+    sim = KappaSimulator(michment.model, tspan=np.linspace(0, 100, 101))
+    res = sim.run(n_runs=2, seed=KAPPA_SEED)
+    assert res.nsims == 2
+
+
+def test_kappa_2initials():
+    sim = KappaSimulator(michment.model, tspan=np.linspace(0, 100, 101))
+    res = sim.run(initials={
+        michment.model.initials[0].pattern: [10, 100],
+        michment.model.initials[1].pattern: [100, 1000]
+    }, seed=KAPPA_SEED)
+    assert res.nsims == 2
+
+
+def test_kappa_2params():
+    base_param_values = np.array(
+        [p.value for p in michment.model.parameters]
+    ).repeat(2).reshape(2, len(michment.model.parameters))
+    sim = KappaSimulator(michment.model, tspan=np.linspace(0, 100, 101))
+
+    res = sim.run(param_values=base_param_values, seed=KAPPA_SEED)
+    assert res.nsims == 2
