1db43e31770584f268e52bb333c47c58e77f9c73
Alex Lubbock <code@alexlubbock.com>
2019-09-20 16:07:20 -0500

    Add multibonds unit test for BNG

--- pysb/tests/test_bng.py
+++ pysb/tests/test_bng.py
@@ -236,6 +236,25 @@ def test_multistate():
         as_complex_pattern(A(a=MultiState('u', 'p'))))
 
 
+@with_model
+def test_multibonds():
+    Monomer('A', ['a'])
+    Monomer('B', ['b'])
+    Parameter('k1', 100)
+    Parameter('A_0', 200)
+    Parameter('B_0', 50)
+    Rule('r1', A(a=None) + A(a=None) + B(b=None) >>
+            A(a=1) % A(a=[1, 2]) % B(b=2), k1)
+    Initial(A(a=None), A_0)
+    Initial(B(b=None), B_0)
+
+    generate_equations(model)
+
+    assert model.species[2].is_equivalent_to(
+        A(a=1) % A(a=[1, 2]) % B(b=2)
+    )
+
+
 def _bng_print(expr):
     return BngPrinter(order='none').doprint(expr)
 
