ba07e1d54f4bf41ce0f04514ee1f33a0f759b105
Alex Lubbock <code@alexlubbock.com>
2020-01-22 16:52:52 -0600

    Remove weave and theano from ScipyOdeSimulator
    Remove legacy weave and theano support from ScipyOdeSimulator.
    The Python exporter has also been updated to use Cython
    rather than weave.

--- pysb/tests/test_exporters.py
+++ pysb/tests/test_exporters.py
@@ -69,7 +69,7 @@ def check_convert(model, format):
     if exported_file is not None:
         if format == 'python':
             # linspace arguments picked to avoid VODE warning
-            exec(exported_file + 'Model().simulate(tspan=numpy.linspace(0,1,501))\n', {'_use_inline': False})
+            exec(exported_file + 'Model().simulate(tspan=numpy.linspace(0,1,501))\n')
         elif format == 'pysb_flat':
             exec(exported_file, {'__name__': model.name})
         elif format == 'sbml':
