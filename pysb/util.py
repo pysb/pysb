f0a75bd48287a1fb9a13b313feee0a2d2455f29f
Fabian Fröhlich <fabian.frohlich@crick.ac.uk>
2026-02-17 14:34:10 +0000

    Fix installation, dot compatibility and remove leftover distutils code (#608)
    - Replaces ez_setup.py with pyproject.toml (from
    https://github.com/pysb/pysb/pull/606)
    - provides compatibility with pydot 4 (requires networkx>=3.6.1 and
    therefore python>=3.11)
    - removes supposedly leftover dead distutils code to ensure
    compatibility with python 3.13+
    - updates doctests (required post-scipy update that changed integration
    code)
    - add setuptools to conda env to make sure Cython can import distutils
    in python 3.12+ (why is this not a Cython dependency?)
    
    It looks like there is a massive blow-up in terms of runtime for the
    tests, it seems this is related to the way pysb is installed (`pip
    install -e .` vs `python setup.py` (not great as it ignores
    dependencies)) but not entirely clear why that would be.
    
    ---------
    
    Co-authored-by: Alex Lubbock <code@alexlubbock.com>

--- pysb/util.py
+++ pysb/util.py
@@ -146,7 +146,12 @@ def _from_pydot(P):
     >>> G = nx.Graph(nx.nx_pydot.from_pydot(A))
 
     """
-    if P.get_strict(None):  # pydot bug: get_strict() shouldn't take argument
+    try: # remove when pydot 3 support is dropped
+        is_strict = P.get_strict() # works for pydot 4+
+    except TypeError:
+        is_strict = P.get_strict(None) # workaround for pydot 3 bug
+
+    if is_strict:
         multiedges = False
     else:
         multiedges = True
