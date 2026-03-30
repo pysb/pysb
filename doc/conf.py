e21933e78922d4e1f1496c88d848ca82572701da
Alex Lubbock <code@alexlubbock.com>
2026-03-30 11:42:47 +0100

    chore(deps): replace mock with unittest.mock, drop unused deps
    * Replace mock with unittest.mock
    * Remove coveralls (CI uses codecov)
    * Add note that cythonize requires setuptools

--- doc/conf.py
+++ doc/conf.py
@@ -13,7 +13,7 @@
 
 import sys, os
 import re
-import mock
+from unittest import mock
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
